b72c77448ac7fd99d1264befd422eb2b7688dcfd
tim <tim@timschupp.de>
2022-11-09 00:56:08 +0100

    the State model

---
+++ back/management/models/state.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class State(models.Model):
+    """
+    This is the base state model for every user 
+    It handles things like email verification, 
+    the users matches, and if the userform is filled
+    """
+
+    # Key...
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+
+    # We love additional Information
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    """ Form page the user is currently on """
+    user_form_page = models.IntegerField()
+
+    """ If the user_form ist filled or not """
+    user_form_state = models.IntegerField()
+
+
+class StateSerializer(serializers.ModelSerializer):
+    """
+    Note: this serializer is not to be used for matches of the current user 
+    This should only be used to expose data of the user to him self or an admin
+    """
+    class Meta:
+        model = State
+        fields = '__all__'
