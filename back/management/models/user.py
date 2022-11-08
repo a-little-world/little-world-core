b50205ba84c0376c9170970b5dff0d28b38e7533
tim <tim@timschupp.de>
2022-11-09 00:55:33 +0100

    added AbstracUser model with hash and added censored user serializer

--- back/management/models/user.py
+++ back/management/models/user.py
@@ -1,9 +1,29 @@
 from django.db import models
+from uuid import uuid4
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    """
+    The default django user model.
+    It is recommended to extend this class
+    make small modifications if required
+    in the settings we set this via 'AUTH_USER_MODEL'
+    """
+    hash = models.CharField(max_length=100, blank=True,
+                            unique=True, default=uuid4)  # type: ignore
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = '__all__'
+
+
+class CensoredUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["hash"]
