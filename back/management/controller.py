7811022ef70cb9250e42cce09c64d8333c128946
tim <tim@timschupp.de>
2022-11-09 00:52:50 +0100

    added important basic control functions

--- back/management/controller.py
+++ back/management/controller.py
@@ -5,6 +5,29 @@ e.g.: Creating a new user, sending a notification to a users etc...
 from .models import UserSerializer
 
 
+def get_user(user, lookup="email"):
+    if lookup == "email":
+        return get_user_by_email(user)
+    elif lookup == "hash":
+        return get_user_by_hash(user)
+    elif lookup == "pk":
+        return get_user_by_pk(user)
+    else:
+        raise Exception(f"lookup '{lookup}' doesn't exist ")
+
+
+def get_user_by_email(email):
+    pass
+
+
+def get_user_by_hash(hash):
+    pass
+
+
+def get_user_by_pk(pk):
+    pass
+
+
 def create_user(
     username,
     email,
