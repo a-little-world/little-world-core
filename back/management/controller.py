8f2b68898032341339c8a2ef6a4835530d74d0b8
tim <tim@timschupp.de>
2022-11-09 01:29:26 +0100

    sketched out some more important controller functions

--- back/management/controller.py
+++ back/management/controller.py
@@ -2,10 +2,17 @@
 This is a controller for any userform related actions
 e.g.: Creating a new user, sending a notification to a users etc...
 """
+from django.contrib.auth import get_user_model
+from django.conf import settings
 from .models import UserSerializer
 
 
 def get_user(user, lookup="email"):
+    """
+    Three ways to look up a user: email, hash or pk
+    pk is obviously the fastest, but generaly we shouldn't have to care
+    we could also add some neat indexing here to speed up further
+    """
     if lookup == "email":
         return get_user_by_email(user)
     elif lookup == "hash":
@@ -17,15 +24,19 @@ def get_user(user, lookup="email"):
 
 
 def get_user_by_email(email):
-    pass
+    return get_user_model().objects.get(email=email)
 
 
 def get_user_by_hash(hash):
-    pass
+    # TODO: here i'm assuming that .get return only one object and throws an error if there are multiple
+    return get_user_model().objects.get(hash=hash)
 
 
+# We accept string input, but this will error if its not convertable to int
 def get_user_by_pk(pk):
-    pass
+    pk = int(pk)
+    # we use .get with primary key so we will always get only one user
+    return get_user_model().objects.get(id=pk)
 
 
 def create_user(
@@ -57,4 +68,33 @@ def create_user(
     ))  # type: ignore
     user_data_serializer.is_valid(raise_exception=True)
 
-    # Step 2
+    # Step 2 ... TODO
+
+
+def match_users(users: set):
+    """ Accepts a list of two users to match """
+    assert len(users) == 2, "Accepts only two users!"
+    # Match ... TODO
+
+
+def unmatch_users(users: set):
+    """ Accepts a list of two users to unmatch """
+    assert len(users) == 2, "Accepts only two users!"
+    # Un-Match ... TODO
+
+
+def get_base_management_user():
+    """
+    Always returns the BASE_MANAGEMENT_USER user
+    """
+    get_user_by_email(settings.BASE_MANAGEMENT_USER_EMAIL)
+
+
+# TODO: can this cause issues when settings not initalized?
+def send_chat_message(to_user, from_user, message):
+    """
+    This can send a chat message from any user to any user
+    this is intended to the used by the BASE_MANAGEMENT_USER
+    so usualy the from_user would be 'get_base_management_user'
+    """
+    # Send a chat message ... TODO
