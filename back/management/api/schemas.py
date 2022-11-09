43c3919ab8d1a51c97adb1cddad39cd1c3d0299c
tim <tim@timschupp.de>
2022-11-09 05:07:20 +0100

    and gracefull fade out of the shema file, seperating this is too much splitting

--- back/management/api/schemas.py
+++ back/management/api/schemas.py
@@ -6,18 +6,13 @@ but you can also extend them how ever you want
 from . import register
 from drf_spectacular.utils import OpenApiParameter, OpenApiExample
 
-registration = dict(
-    # extra parameters added to the schema
-    parameters=[
-        OpenApiParameter(
-            name=param, description=f'{param} for Registration',
-            required=True, type=str)
-        for param in register.Register.required_args
-    ],
-    description='Little World Registration API called with data from the registration form',
-    auth=None,
-    operation_id=None,
-    operation=None,
-    methods=["POST"],
-    request=register.RegistrationSerializer(many=False)
-)
+
+def get_register_schema(params, serializer):
+    return dict(
+        description='Little World Registration API called with data from the registration form',
+        auth=None,
+        operation_id=None,
+        operation=None,
+        methods=["POST"],
+        request=serializer
+    )
