b3ed0dd36d9df440c3dbe5e7c6692d7226103d67
tim <tim@timschupp.de>
2022-11-10 15:08:48 +0100

    first draft of the tracking module

--- back/tracking/utils.py
+++ back/tracking/utils.py
@@ -1,10 +1,47 @@
 from functools import partial, wraps
+from .models import Event
 
 
-def _dispath_event_tracking(f, caller="anonymous", event_name="", tags=[]):
+possible_metadata = [
+    "request",
+    "user",
+    "email"
+]
+
+
+def _dispath_event_tracking(f,
+                            caller="anonymous",
+                            name="",
+                            type: int = Event.EventTypeChoices.MISC,
+                            tags=[],
+                            track_arguments=[],
+                            censor_args=False,
+                            censor_kwargs=[]
+                            ):
+    """
+    Wrap an arbitray function to track 
+    you can provide an event type, name, caller and some tags
+    the rest of data is collected automaticly.
+    It also tries to collect a bunch of meta data if present.
+    For sensitive data use censor_kwargs / censor_args
+    """
 
     @wraps(f)
     def run(*args, **kwargs):
+        metadata = {
+            "kwargs": {arg: kwargs.get(arg) for arg in track_arguments},
+            "args": args
+        }
+        if type == Event.EventTypeChoices.MISC:
+            metadata['request']
+
+        Event.objects.create(
+            # `time` is set automaticly
+            tags=tags,
+            func=f.__name__,
+            name=name,
+            metadata={}
+        )
         return f(*args, **kwargs)
     return run
 
