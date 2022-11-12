94bb70480c7823a90a7680ff5b31b1af1628bc46
tim <tim@timschupp.de>
2022-11-13 00:04:45 +0100

    sketch for a email viewer view

--- back/emails/views.py
+++ back/emails/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from tracking import utils
+from django.views import View
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.utils.translation import gettext as _
+from tracking.models import Event
 
-# Create your views here.
+
+class ViewEmail(UserPassesTestMixin, View):
+    login_url = '/login'
+    redirect_field_name = 'next'
+
+    @utils.track_event(name=_("Email Viewed"), event_type=Event.EventTypeChoices.REQUEST, tags=["frontend"])
+    def get(self, request, **kwargs):
+        pass
