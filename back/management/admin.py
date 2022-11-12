75d6870d22d27b50fe55f7a5f6cc23a71de03eea
tim <tim@timschupp.de>
2022-11-12 13:52:41 +0100

    display the user hash

--- back/management/admin.py
+++ back/management/admin.py
@@ -44,7 +44,7 @@ class UserAdmin(DjangoUserAdmin):
     ]
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password', 'hash')}),
         (_('Permissions'), {
          'fields': ('is_active', 'is_staff', 'is_superuser')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
