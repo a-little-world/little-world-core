a97b9bc3c2664a317e2f4e174064e747b44e3a18
tim <tim@timschupp.de>
2022-11-12 16:35:34 +0100

    registered Event model in admin pannel

--- back/tracking/admin.py
+++ back/tracking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+
+
+@admin.register(Event)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('hash', 'time', 'caller', 'type')
+    search_fields = ('hash', 'time', 'caller', 'type')
