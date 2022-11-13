20b849aef581221b3cf9f7b6c8a19d2adf5df3c5
tim <tim@timschupp.de>
2022-11-13 15:57:09 +0100

    first draft of the email render view

--- back/emails/views.py
+++ back/emails/views.py
@@ -4,12 +4,23 @@ from django.views import View
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.utils.translation import gettext as _
 from tracking.models import Event
+from .mails import get_mail_data_by_name
 
 
 class ViewEmail(UserPassesTestMixin, View):
     login_url = '/login'
     redirect_field_name = 'next'
 
+    def test_func(self):
+        return self.request.user.is_staff
+
     @utils.track_event(name=_("Email Viewed"), event_type=Event.EventTypeChoices.REQUEST, tags=["frontend"])
     def get(self, request, **kwargs):
-        pass
+
+        if not 'mail_name':
+            pass  # TODO: throm error
+
+        mail_name = kwargs.get('mail_name')
+        print("TBS: " + str(mail_name))
+        mail_data = get_mail_data_by_name(mail_name)
+        return render(request, mail_data['template'], {})
