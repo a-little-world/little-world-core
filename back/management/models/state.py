9843d827999ef8daa8fe2c76132083fb29c76b5a
tim <tim@timschupp.de>
2022-11-13 01:25:11 +0100

    added email verification code generation and checking

--- back/management/models/state.py
+++ back/management/models/state.py
@@ -1,5 +1,9 @@
 from django.db import models
 from .user import User
+import json
+import base64
+from back import utils
+import random
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
@@ -10,7 +14,6 @@ class State(models.Model):
     It handles things like email verification, 
     the users matches, and if the userform is filled
     """
-
     # Key...
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -29,13 +32,58 @@ class State(models.Model):
     user_form_state = models.IntegerField(
         default=UserFormStateChoices.UNFILLED)
 
+    # Just some hash for verifying the email
+    email_auth_hash = models.CharField(default=utils._double_uuid)
+    email_auth_pin = models.IntegerField(
+        # By wrapping in lambda this will get called when the model is created
+        # and not at server start, then we get better randomization maybe
+        # Also this conveniently inialized the pin
+        default=lambda: random.randint(100000, 999999))
+
+    email_authenticated = models.BooleanField(default=False)
+
+    def is_email_verified(self):
+        return self.email_authenticated
+
+    def check_email_auth_pin(self, pin):
+        """
+        checks email verification pin, this shall only be used it the user is logged in!
+        """
+        _check = pin == self.email_auth_pin
+        if _check:
+            self.email_authenticated = True
+            self.save()
+        return _check
+
+    def check_email_auth_code_b64(self, code):
+        """
+        checks the email verification credentials
+        note this is will be used it authentication through a link ( with out being logged in )
+        """
+        _data = self.decode_email_auth_code_b64(code)
+        # u: user hash, h: email verification hash, p: email verification pin
+        _check = _data["u"] == self.user.hash and \
+            _data["h"] == self.email_auth_hash and \
+            int(_data["p"]) == self.email_auth_pin
+        if _check:
+            self.email_authenticated = True
+            self.save()
+        return _check
+
+    def get_email_auth_code_b64(self):
+        return base64.encodestring(json.dumps({
+            "u": self.user.hash, "h": self.email_auth_hash, "p": self.email_auth_pin}).encode())
+
+    def decode_email_auth_code_b64(self, str_b64):
+        return json.loads(base64.decodestring(str_b64))
+
 
 class StateSerializer(serializers.ModelSerializer):
     """
     Note: this serializer is not to be used for matches of the current user 
     This should only be used to expose data of the user to him self or an admin
     """
-    hash = serializers.SerializerMethodField()
+    usr_hash = serializers.SerializerMethodField()
 
     def get_hash(self, obj):
         return obj.user.hash
