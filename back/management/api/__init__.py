d8a8ad3d3938c8007b35fb0f20e1265abdad43ad
tim <tim@timschupp.de>
2022-11-14 14:11:05 +0100

    updated api __init__

--- back/management/api/__init__.py
+++ back/management/api/__init__.py
@@ -1,2 +1,2 @@
-from . import user_data, register, admin, user
-__all__ = ["user_data", "register", "admin", "user"]
+from . import user_data, register, admin, user, profile
+__all__ = ["user_data", "register", "admin", "user", "profile"]
