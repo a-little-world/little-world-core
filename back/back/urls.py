0607c488fdad822958b43807c9ee118a325bbe74
tim <tim@timschupp.de>
2022-11-15 01:18:00 +0100

    relocated api shema to back/urls

--- back/back/urls.py
+++ back/back/urls.py
@@ -1,3 +1,4 @@
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -16,7 +17,15 @@ urlpatterns = [
     path("cookies/", include("cookie_consent.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
 if settings.BUILD_TYPE in ['staging', 'development']:
+    # TODO: these views should also be blocked for admin only acess
     urlpatterns += [
         path('db/', include('django_spaghetti.urls')),
+        # Don't expose the api shemas in production!
+        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('api/schema/swagger-ui/',
+             SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path('api/schema/redoc/',
+             SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     ]
