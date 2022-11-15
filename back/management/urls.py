ad6647bfd7cbdedac1da720fdd47de2aecef6a6d
tim <tim@timschupp.de>
2022-11-15 01:39:14 +0100

    added notification apis and moved shemas to back.urls

--- back/management/urls.py
+++ back/management/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from . import views
 from back.utils import _api_url
 from rest_framework import routers
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
 router = routers.SimpleRouter()
@@ -12,13 +11,17 @@ router = routers.SimpleRouter()
 api_routes = [
     # User
     path(_api_url('user_data'), api.user_data.UserData.as_view()),
+
     path(_api_url('register'), api.register.Register.as_view()),
     path(_api_url('user'), api.user_data.SelfInfo.as_view()),
     path(_api_url('profile'),
          api.profile.ProfileViewSet.as_view({"post": "partial_update"})),
 
-    # TODO this should be a viewset for the whole profile model!
-    # path(_api_url('user/profile'), api.user_data.SelfInfo.as_view()),
+    path(_api_url('notifications'),
+         api.notify.NotificationGetApi.as_view()),
+    path(_api_url('notification/<str:action>', end_slash=False),
+         api.notify.NotificationActionApi.as_view()),
+
 
     # e.g.: /user/verify/email/Base64{d=email&u=hash&k=pin:hash}
     path(_api_url('user/verify/email/<str:auth_data>'),  # TODO create verify email api
@@ -34,17 +37,10 @@ api_routes = [
 ]
 
 urlpatterns = [
-    # Frontends:
 
     *api_routes,  # Add all API routes from above
 
-    *([  # Don't expose the api shemas in production!
-        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-        path('api/schema/swagger-ui/',
-             SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-        path('api/schema/redoc/',
-             SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    ] if settings.BUILD_TYPE in ['development', 'staging'] else []),
 
+    # Frontends:
     re_path(fr'^app/(?P<path>.*)$', views.MainFrontendView.as_view()),
 ]
