c7d739c9f7f3e8af8e5fa03a39699d682d188113
tim <tim@timschupp.de>
2022-11-17 13:56:56 +0100

    rename and fix frontend envs

--- front/env_apps/cookie_banner_frontend.local.js
+++
@@ -1,3 +0,0 @@
-exports.DEVELOPMENT = true; // this is more like FRONTEND_LOCAL_DEVELOPENT ( it's ment to be false, when use in backend local development )
-exports.PRODUCTION = false; // This allows to render either by path or by callback name
-exports.BACKEND_URL = this.DEVELOPMENT ? "http://localhost:3333" : "";
