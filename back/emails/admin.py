69f2e8154d78f7145d5776afa5431ed692bb5435
tim <tim@timschupp.de>
2022-11-20 18:06:43 +0100

    display also if the email sending was sucessfull

--- back/emails/admin.py
+++ back/emails/admin.py
@@ -4,4 +4,5 @@ from .models import EmailLog
 
 @admin.register(EmailLog)
 class EmailLogAdmin(admin.ModelAdmin):
-    list_display = ("receiver", "template", "time", "sender", "data")
+    list_display = ("receiver", "sucess", "template",
+                    "time", "sender", "data")
