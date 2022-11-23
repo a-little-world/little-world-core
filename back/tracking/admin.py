1d05c4d4574b28e234def9a9ee9df72d6a9f4fec
tim <tim@timschupp.de>
2022-11-23 15:55:07 +0100

    updated tracking admin, link to user now

--- back/tracking/admin.py
+++ back/tracking/admin.py
@@ -2,12 +2,29 @@ from django.contrib import admin
 from .models import Event
 
 # Register your models here.
+from django.template.defaultfilters import escape
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from management.models.user import User
 
 
 @admin.register(Event)
-class StateAdmin(admin.ModelAdmin):
-    list_display = ('_abr_hash', 'name', 'type', 'caller',
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('_abr_hash', 'name', 'type', 'user_ref',
                     'time', 'tags', 'func', 'metadata')
     search_fields = ('_abr_hash', 'name', 'type', 'caller',
                      'time', 'tags', 'func', 'metadata')
     list_filter = ("caller",)
+
+    readonly_fields = ('user_ref',)
+
+    def user_ref(self, obj):
+        if obj.caller is not None:
+            from django.shortcuts import resolve_url
+            from django.contrib.admin.templatetags.admin_urls import admin_urlname
+            url = resolve_url(admin_urlname(
+                User._meta, 'change'), obj.caller.pk)
+            return mark_safe(f'<a href="{url}">{escape(obj.caller.email)}</a>')
+
+        else:
+            return mark_safe('no caller')
