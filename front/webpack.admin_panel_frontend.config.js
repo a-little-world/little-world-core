c193d14b94c53a355d3ba1a283520c88786c8e21
tim <tim@timschupp.de>
2022-11-24 16:58:38 +0100

    autogenerated webpack config for the admin panel

---
+++ front/webpack.admin_panel_frontend.config.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker');
+const CompressionPlugin = require('compression-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+var config = function (env) {
+  var publicPath = '/static/dist/admin_panel_frontend/';
+  var devTool = env.DEV_TOOL;
+  // It is always assumed that the backend is mounted at /back
+  var outputPath = '../back/static/dist/admin_panel_frontend';
+  var entry = './apps/admin_panel_frontend';
+  var entryPoint = `${entry}/src/index.js`;
+  var debug = env.DEBUG === '1';
+
+  return {
+    context: __dirname,
+    entry: {
+      staticfiles: entryPoint,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'apps/admin_panel_frontend/src/'),
+        '@django': path.resolve(__dirname, '../back/static/'),
+      },
+    },
+    output: {
+      path: path.join(__dirname, outputPath),
+      filename: '[name]-[hash].js',
+      publicPath: publicPath,
+    },
+
+    plugins: [
+      new BundleTracker({
+        filename: path.join(
+          __dirname,
+          './admin_panel_frontend.webpack-stats.json'
+        ),
+      }),
+      new CompressionPlugin(),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, `${entry}/public`),
+            to: path.join(__dirname, outputPath),
+          },
+        ],
+      }),
+    ],
+    devtool: devTool,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|tsx|ts)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+          resolve: {
+            extensions: ['.js', '.jsx'],
+          },
+          include: [
+            path.resolve(__dirname, 'apps/admin_panel_frontend/src'),
+          ],
+        },
+        {
+          test: /\.svg$/,
+          issuer: /\.jsx?$/,
+          use: [
+            'babel-loader',
+            {
+              loader: 'react-svg-loader',
+              options: {
+                svgo: {
+                  plugins: [{ removeTitle: false }],
+                  floatPrecision: 2,
+                },
+                jsx: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif|ttf)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash:8].[ext]',
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  };
+};
+
+module.exports = (env, argv) => {
+  const conf = config(env);
+  console.log(conf);
+  return conf;
+};
