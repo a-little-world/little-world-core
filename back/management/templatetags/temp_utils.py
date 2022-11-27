ae4439c8e0c76535f9d2964f8f7962851973351e
tim <tim@timschupp.de>
2022-11-27 16:43:58 +0100

    added template util that can get the base_url

--- back/management/templatetags/temp_utils.py
+++ back/management/templatetags/temp_utils.py
@@ -1,7 +1,13 @@
 from django import template
+from django.conf import settings
 register = template.Library()
 
 
 @register.simple_tag
 def create_dict(str_dict):
     return eval(str_dict)
+
+
+@register.simple_tag
+def get_base_url():
+    return settings.BASE_URL
