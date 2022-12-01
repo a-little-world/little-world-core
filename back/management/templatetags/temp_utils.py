7090e810e13ddde5952927cc9291636786a1ca76
tim <tim@timschupp.de>
2022-12-01 15:50:15 +0100

    template util to load the api translations

--- back/management/templatetags/temp_utils.py
+++ back/management/templatetags/temp_utils.py
@@ -1,5 +1,7 @@
 from django import template
 from django.conf import settings
+import json
+from ..api.trans import get_trans_as_tag_catalogue
 register = template.Library()
 
 
@@ -11,3 +13,15 @@ def create_dict(str_dict):
 @register.simple_tag
 def get_base_url():
     return settings.BASE_URL
+
+
+@register.simple_tag
+def get_api_translations(request):
+    """
+    A helper tag that returns the api trasnlations  
+    This can be used by frontends to dynamicly change error translation lanugages without resending requrests
+    """
+    return json.dumps({
+        "en": get_trans_as_tag_catalogue(request, "en"),
+        "de": get_trans_as_tag_catalogue(request, "de")
+    })
