e65fbde56e8568ef359e5dc393b2e378729cfc0e
tim <tim@timschupp.de>
2022-12-02 20:44:08 +0100

    one command is sufficient to start celery worker and beat

--- back/entry.sh
+++ back/entry.sh
@@ -1,5 +1,5 @@
 # This is the general entry point for server startup ( dev )
 # TODO: in the future these procesees should be deomonized or handled by supervisord
-celery -A back worker --loglevel=info &
-celery -A back beat --loglevel=info &
+celery -A back worker --beat --loglevel=info &
+#celery -A back beat --loglevel=info &
 uvicorn back.asgi:application --reload --port 8000 --host 0.0.0.0
