3ee3751e483bcdde7394c7d71cac51dba4d8d7f7
tim <tim@timschupp.de>
2022-12-04 23:13:16 +0100

    added extra management middleware that cna be used to overwite session language with a custom html header

--- back/management/middleware.py
+++ back/management/middleware.py
@@ -1,3 +1,4 @@
+from django.utils import translation
 from django.template import RequestContext
 from django.shortcuts import render
 from django.conf import settings
@@ -48,3 +49,22 @@ class AdminPathBlockingMiddleware:
             if _is_blocked_route(path):
                 return _404_if_not_staff(request, self.get_response)
         return self.get_response(request)
+
+
+USE_TAG_HEADER = "HTTP_X_USETAGSONLY"
+
+
+class OverwriteSessionLangIfAcceptLangHeaderSet:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """
+        We add our own http header that can be used to for usage of **only** translations tags for the frontends!
+        """
+        if USE_TAG_HEADER in request.META and request.META[USE_TAG_HEADER]:
+            with translation.override("tag"):
+                return self.get_response(request)
+        else:
+            return self.get_response(request)
