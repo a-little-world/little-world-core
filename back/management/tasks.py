b327e750a000b827ee15d39a334a0efcef090d5a
tim <tim@timschupp.de>
2022-12-05 01:06:04 +0100

    moved task for directional mapping score calulation to celery.py

--- back/management/tasks.py
+++ back/management/tasks.py
@@ -1,3 +1,4 @@
+from .models import User
 import datetime
 from django.utils.translation import pgettext_lazy
 from .models.community_events import CommunityEvent, CommunityEventSerializer
@@ -102,3 +103,18 @@ Selbst habe ich vier Jahre im Ausland gelebt, von Frankreich bis nach China. Den
     # TODO: upload default image!
     usr.profile.save()
     return "sucessfully filled base management user profile"
+
+
+@shared_task
+def calculate_directional_matching_score_background(usr):
+    """
+    This is the backend task for calculating a matching score. 
+    This will *automaticly* be executed everytime a users changes his user form
+    run with calculate_directional_matching_score_background.delay(usr)
+    """
+
+    for other_usr in User.objects.all().exclude(usr=usr):
+        # We do loop over all users, but for most users the score calculation will abort quickly
+        # e.g.: if the user is a volunteer and the 'other_usr' is also a volunteer
+        # then the score calculation would abbort imediately and return 'matchable = False'
+        pass
