0757b1391c0e7c426ab0403dfd2e887a83d8efae
tim <tim@timschupp.de>
2022-12-05 16:25:29 +0100

    import translations dialog only the moment it is needed

--- back/management/templatetags/temp_utils.py
+++ back/management/templatetags/temp_utils.py
@@ -1,7 +1,6 @@
 from django import template
 from django.conf import settings
 import json
-from ..api.trans import get_trans_as_tag_catalogue
 register = template.Library()
 
 
@@ -17,6 +16,7 @@ def get_base_url():
 
 @register.simple_tag
 def get_api_translations(request):
+    from ..api.trans import get_trans_as_tag_catalogue
     """
     A helper tag that returns the api trasnlations  
     This can be used by frontends to dynamicly change error translation lanugages without resending requrests
