3f776e96860e888834ca5616e72182117a9ebbbf
tim <tim@timschupp.de>
2022-12-11 11:52:56 +0100

    improved matching score calulation, added option to render results in a neat markdown table

--- back/management/matching/matching_score.py
+++ back/management/matching/matching_score.py
@@ -10,6 +10,10 @@ from .score_tables import (
     partner_location_msg
 )
 
+from .score_table_lookup import (
+    check__plz_distance_matching_score
+)
+
 
 def _generate_table_scoring(
     usr1,
@@ -31,8 +35,8 @@ def _generate_table_scoring(
     |  0  | 25 | 30 | 20 | 20 |
     |  1  |  0 | 25 |  0 | 0  |
     |  2  | 0  | 0  | 25 | 0  |
-    |  3  | 0  | 0  | 0  | 25 | 
-    In this case the index correspond to the field 'profile.helping_group' 
+    |  3  | 0  | 0  | 0  | 25 |
+    In this case the index correspond to the field 'profile.helping_group'
     and the limiting condition is 'volunter or helper'
     condition 'True' always maps to y-axis
     condition 'False' always maps to x-axis
@@ -48,16 +52,10 @@ def _generate_table_scoring(
     # Check which values those people have set for the specific field
     values = [value_lookup(usr1), value_lookup(usr2)]
 
-    # Now convert the numpy array to a graph
-    graph = nx.from_numpy_matrix(table, create_using=nx.MultiGraph)
-    text_graph = None
-    if table_msgs:
-        text_graph = nx.from_numpy_matrix(
-            table_msgs, create_using=nx.MultiGraph)
-
     _matchable = True
-    _usr1_to_usr2_score = graph[values[axes[0]]][values[axes[1]]][0]['weight']
-    # This is mostry an integer but it can also be 'X' which would mean they are unmatchable!
+    table_key = (values[axes[0]], values[axes[1]])
+    _usr1_to_usr2_score = table[table_key]
+    # Choices are always strings now!
     if _usr1_to_usr2_score.lower() == "x":
         _matchable = False
     else:
@@ -65,13 +63,12 @@ def _generate_table_scoring(
         # so it if wher not an integer we should error here:
         _usr1_to_usr2_score = int(_usr1_to_usr2_score)
 
-    _msg = text_graph[values[axes[0]]][values[axes[1]]
-                                       ][0]['weight'] if text_graph else None
+    _msg = table_msgs[table_key] if table_msgs else ""
     # Always return a tripel ( is_matchable, score, score message )
-    return _matchable, _usr1_to_usr2_score, _msg
+    return _matchable, _usr1_to_usr2_score, _msg, {"values": (table_key[0], table_key[1])}
 
 
-def dispatch_table_score(limiting_condition, value_lookup, table, table_msgs):
+def dispatch_table_score(limiting_condition, value_lookup, table, table_msgs=None):
     # This is a wrapper that converts a table score function
     # in a function that only need usr1, usr2 as input
     # So basicly it sores the limiting conditon and the tables
@@ -86,6 +83,10 @@ LIMITING_CONDITIONS = dict(
     learner=lambda usr: usr.profile.user_type == Profile.TypeChoices.VOLUNTEER,
 )
 
+SCORING_FUNCTIONS = dict(  # This matches models.matching_score.TabaseScoring.ScoreFunctions
+    postal_code_distance_check=check__plz_distance_matching_score
+)
+
 
 """
 This holds all the scoring functions,
@@ -93,6 +94,32 @@ per default they are *all* directional!
 But there are some helpers you can use if you want to define a symetrical score
 e.g.: _generate_table_scoring()
 """
+
+
+def get_scoring_from_latest_table_model():
+    from ..models.matching_scores import ScoreTableSource
+    scores = ScoreTableSource.get_latest()
+    # TODO fallback to default if no latest table is found
+    assert scores, "No 'latests' scoring table!"
+
+    return dict(
+        target_group=dispatch_table_score(
+            limiting_condition=LIMITING_CONDITIONS['learner'],
+            value_lookup=lambda usr: usr.profile.target_group,
+            table=scores.get_table_field_as_graph_dict('target_group_scores'),
+            table_msgs=scores.get_table_field_as_graph_dict('target_group_messages')),
+        language_level=dispatch_table_score(
+            limiting_condition=LIMITING_CONDITIONS['learner'],
+            value_lookup=lambda usr: usr.profile.lang_level,
+            table=scores.get_table_field_as_graph_dict('language_level_scores')),
+        partner_location=dispatch_table_score(
+            limiting_condition=LIMITING_CONDITIONS['learner'],
+            value_lookup=lambda usr: usr.profile.partner_location,
+            table=scores.get_table_field_as_graph_dict('partner_location_scores')),
+        partner_distance=SCORING_FUNCTIONS['postal_code_distance_check'],
+    )
+
+
 SCORINGS = dict(  # These nice python 3 dicts are ordered! so this also defines the order of calcuation
 
     helping_group=dispatch_table_score(
@@ -113,8 +140,39 @@ SCORINGS = dict(  # These nice python 3 dicts are ordered! so this also defines 
         table=partner_location,
         table_msgs=partner_location_msg),
 
+    partner_distance=SCORING_FUNCTIONS['postal_code_distance_check'],
+
     # TODO: insert the other scorings ...
 )
+
+
+def scoring_result_dict_as_markdown_table(scoring_results_dict):
+    from pytablewriter import MarkdownTableWriter
+    from django.utils.html import escape
+    import json
+    import shlex
+    headers = ["scoring name", "v1", "v2", "usr1 -> usr2 score",
+               "score message", "is matchable"]
+
+    values = []
+    for key in scoring_results_dict:
+        v = []
+        if "values" in scoring_results_dict[key]["meta"]:
+            v = [scoring_results_dict[key]["meta"]["values"][0],
+                 scoring_results_dict[key]["meta"]["values"][1]]
+        else:
+            v = ["-", "-"]
+        values.append([
+            key, v[0], v[1],
+            scoring_results_dict[key]['score'], scoring_results_dict[key]['msg'],
+            scoring_results_dict[key]['matchable']
+        ])
+    writer = MarkdownTableWriter(
+        table_name="example_table",
+        headers=headers,
+        value_matrix=values,
+    )
+    return writer.dumps()
 
 
 def calculate_directional_matching_score(
@@ -122,7 +180,7 @@ def calculate_directional_matching_score(
     return_on_nomatch=False
 ):
     """
-    A general function that can calucate the directionmal matching score for two users 
+    A general function that can calucate the directionmal matching score for two users
     This is held very generic, and based on aboves SCORINGS dict
     we alsoways wan't to keep this rather abstract so we can easily modify this in the future
 
@@ -131,8 +189,20 @@ def calculate_directional_matching_score(
     _messages = {}  # Messenges will always be indexed by field name!
     _score = 0
     _matchable = True
-    for scoring in SCORINGS:
-        matchable, score, msg = SCORINGS[scoring](usr1, usr2)
+
+    scoring_results_dict = {}
+
+    _SCORINGS = get_scoring_from_latest_table_model()
+    for scoring in _SCORINGS:
+        matchable, score, msg, meta = _SCORINGS[scoring](usr1, usr2)
+
+        scoring_results_dict[scoring] = dict(
+            matchable=matchable,
+            score=score,
+            msg=msg,
+            meta=meta,
+        )
+
         if not matchable:
             _matchable = False
         _score += score
@@ -141,4 +211,11 @@ def calculate_directional_matching_score(
             return _matchable, _score, _messages
     # Incase you set return_on_nomatch=False,
     # you will get the full summary even if they are not matchable!
-    return _matchable, _score, _messages
+    scoring_results_dict['**total_score**'] = dict(
+        matchable=_matchable,
+        score=f'**{_score}**',
+        msg="Total score",
+        meta={},
+    )
+    # print(scoring_result_dict_as_markdown_table(scoring_results_dict))
+    return _matchable, _score, _messages, scoring_results_dict
