632dddc19398fca109f0c34e19d5d4afe1a27784
tim <tim@timschupp.de>
2022-12-12 16:01:02 +0100

    added more functionality to the twilio handler

--- back/management/twilio_handler.py
+++ back/management/twilio_handler.py
@@ -16,8 +16,8 @@ def _get_client():
 
 
 def _get_status_url():
-    # TODO: add /twilio_callback url
-    return settings.BASE_URL + "/api/twilio_callback"
+    callback_url = settings.BASE_URL + "/api/video_rooms/twilio_callback"
+    return callback_url
 
 
 def _get_token(identity):
@@ -35,10 +35,23 @@ def make_room(name):
     the status_callback_url is where twilio make callback api calls to
     """
     client = _get_client()
+    room_type = 'go'  # TODO: we can have max 245 of these
     client.video.rooms.create(unique_name=name,
-                              type=name,
+                              type=room_type,
                               media_region='de1',
                               status_callback=_get_status_url())
+
+
+def complete_room(name):
+    client = _get_client()
+    client.video.rooms(name).update(status='completed')
+
+
+def complete_room_if_empty(name):
+    room = get_rooms(name)[0]
+    participants = room.participants.list(status='connected')
+    if len(participants) == 0:
+        complete_room(name)
 
 
 def get_rooms(name):
@@ -49,6 +62,12 @@ def get_rooms(name):
 def room_exists(name):
     rooms = get_rooms(name)
     return len(rooms) > 1
+
+
+def get_room_or_create(name):
+    if not room_exists(name):
+        make_room(name)
+    return get_rooms(name)
 
 
 def get_usr_auth_token(room_name, user_identity):
