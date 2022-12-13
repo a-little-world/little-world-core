78c5de286ba5e6a527d2d78dbfac904049612479
tim <tim@timschupp.de>
2022-12-13 19:46:28 +0100

    added a buch of options to the admin dropdown menu

--- back/back/settings.py
+++ back/back/settings.py
@@ -71,6 +71,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #*(['django.contrib.sessions'] if IS_PROD or IS_STAGE else []),
+
+
 ]
 print(f'Installed apps:\n' + '\n- '.join(INSTALLED_APPS))
 
@@ -495,6 +498,9 @@ JAZZMIN_SETTINGS = {
         {"name": "Repo", "url": "https://github.com/a-little-world/little-world-backend",
             "new_window": True},
 
+        {"name": "DB shema", "url": "/db",
+            "new_window": True},
+
         {"name": "Admin Chat", "url": "/admin_chat",
             "new_window": True},
 
@@ -512,6 +518,21 @@ JAZZMIN_SETTINGS = {
     "usermenu_links": [
         {"name": "AdminChat", "url": f"{BASE_URL}/admin_chat",
             "new_window": True},
+        {"name": "Home",  "url": "/app",
+            "permissions": ["auth.view_user"]},
+        {"name": "Repo", "url": "https://github.com/a-little-world/little-world-backend",
+            "new_window": True},
+        {"name": "DB shema", "url": "/db",
+            "new_window": True},
+        {"name": "Admin Chat", "url": "/admin_chat",
+            "new_window": True},
+        {"name": "Docs", "url": "/static/docs",
+            "new_window": True},
+        {"name": "API", "url": "/api/schema/swagger-ui/",
+            "new_window": True},
+        {"name": "Emails", "url": "/emails/welcome",
+            "new_window": True},
+        {"event": "tracking"},
     ],
     "show_sidebar": True,
     "navigation_expanded": True,
