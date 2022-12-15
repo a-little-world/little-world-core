19656382195a1282069e7b8cfa53a6f602ae21e5
tim <tim@timschupp.de>
2022-12-15 16:01:08 +0100

    registered cookies api

--- back/management/api/__init__.py
+++ back/management/api/__init__.py
@@ -1,3 +1,3 @@
-from . import user_data, register, admin, user, profile, notify, trans, community_events, twilio
+from . import user_data, register, admin, user, profile, notify, trans, community_events, twilio, cookies
 __all__ = ["user_data", "register", "admin",
-           "user", "profile", "notify", "trans", "twilio"]
+           "user", "profile", "notify", "trans", "twilio", "cookies"]
