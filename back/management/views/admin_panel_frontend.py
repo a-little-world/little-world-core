e2141d243944ad2313adfb85c7d2b5743a16e8a2
tim <tim@timschupp.de>
2022-12-20 02:23:04 +0100

    added pagination and a buch of extra infos to the admin panel

--- back/management/views/admin_panel_frontend.py
+++ back/management/views/admin_panel_frontend.py
@@ -7,10 +7,6 @@ from ..models import User
 from django.core.paginator import Paginator
 import json
 
-# TODO: set here for now they should be part of the params later!
-paginate_by = 50
-page = 1
-
 
 @user_passes_test(lambda u: u.is_staff)
 def admin_panel(request):
@@ -32,12 +28,32 @@ def admin_panel(request):
         filtered_user_list = get_users_by_slug_filter(
             filter_slug=filter, user_to_filter=filtered_user_list)
 
+    page = GET.get("page", 1)
+    paginate_by = GET.get("paginate_by", 50)
+
     # Yes this view is also paginated!
-    # Though you can't yet change the page TODO
-    pages = Paginator(filtered_user_list, paginate_by).page(page)
+    paginator = Paginator(filtered_user_list, paginate_by)
+    pages = paginator.page(page)
 
     user_list_data = [get_user_data(
         p, is_self=True, admin=True, include_options=False) for p in pages]
 
+    extra_info = {
+        "paginate_by": paginate_by,
+        "page": page,
+        "num_pages": pages.paginator.num_pages,
+        "results_total": len(filtered_user_list),
+    }
+
+    # It is also possible to pass two selectiosn s1=1&s2=2
+    if 's1' in GET:
+        extra_info['s1'] = GET['s1']
+
+    if 's2' in GET:
+        extra_info['s2'] = GET['s2']
+
     return render(request, "admin_panel_frontend.html",
-                  {"user_data": json.dumps(user_list_data, cls=CoolerJson)})
+                  {"user_data": json.dumps({
+                      "user_list": user_list_data,
+                      "extra_info": extra_info,
+                  }, cls=CoolerJson)})
