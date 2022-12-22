2f50ee949463ba5dd148427239224c0d24f742cf
tim <tim@timschupp.de>
2022-12-22 18:33:25 +0100

    moved pagination logic for slugfilters to slugfilters api

--- back/management/views/admin_panel_frontend.py
+++ back/management/views/admin_panel_frontend.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 from back.utils import CoolerJson
 from ..api.user_data import get_user_data
-from ..api.user_slug_filter_lookup import get_users_by_slug_filter
+from ..api.user_slug_filter_lookup import get_users_by_slug_filter, get_filter_slug_filtered_users_multiple_paginated
 from ..models import User
 from django.core.paginator import Paginator
 import json
@@ -23,28 +23,11 @@ def admin_panel(request):
         # This also defines the default filter!
         filters = [GET.get("filter", "state.user_form_page:is:0")]
 
-    filtered_user_list = list(User.objects.all())
-    for filter in filters:
-        filtered_user_list = get_users_by_slug_filter(
-            filter_slug=filter, user_to_filter=filtered_user_list)
-
-    page = GET.get("page", 1)
-    paginate_by = GET.get("paginate_by", 50)
-
-    # Yes this view is also paginated!
-    paginator = Paginator(filtered_user_list, paginate_by)
-    pages = paginator.page(page)
-
-    user_list_data = [get_user_data(
-        p, is_self=True, admin=True, include_options=False) for p in pages]
-
-    extra_info = {
-        "paginate_by": paginate_by,
-        "page": page,
-        "num_pages": pages.paginator.num_pages,
-        "results_total": len(filtered_user_list),
-    }
-
+    user_list_data, extra_info = get_filter_slug_filtered_users_multiple_paginated(
+        filters=filters,
+        page=GET.get("page", 1),
+        paginate_by=GET.get("paginate_by", 50)
+    )
     # It is also possible to pass two selectiosn s1=1&s2=2
     if 's1' in GET:
         extra_info['s1'] = GET['s1']
