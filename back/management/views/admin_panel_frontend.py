cda3f269b7004c1592ae5f4a9b96b0b24f4295e8
tim <tim@timschupp.de>
2022-12-23 03:30:19 +0100

    added suggestions to extra info if s1 is set in admin panel

--- back/management/views/admin_panel_frontend.py
+++ back/management/views/admin_panel_frontend.py
@@ -4,6 +4,7 @@ from back.utils import CoolerJson
 from ..api.user_data import get_user_data
 from ..api.user_slug_filter_lookup import get_users_by_slug_filter, get_filter_slug_filtered_users_multiple_paginated
 from ..models import User
+from .. import controller
 from django.core.paginator import Paginator
 import json
 
@@ -31,6 +32,15 @@ def admin_panel(request):
     # It is also possible to pass two selectiosn s1=1&s2=2
     if 's1' in GET:
         extra_info['s1'] = GET['s1']
+        user = controller.get_user_by_hash(GET['s1'])
+
+        # If we have 's1' we even try to list suggestions
+        from ..models.matching_scores import MatchinScore
+        suggestions = MatchinScore.matching_suggestion_from_database(user)
+        suggested_users = [get_user_data(
+            controller.get_user_by_pk(int(suggestion["from_usr"]))) for suggestion in suggestions]
+        extra_info['suggested_users'] = suggested_users
+        extra_info['suggestions'] = suggestions
 
     if 's2' in GET:
         extra_info['s2'] = GET['s2']
