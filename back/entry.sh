3ad5dc9ce8297536649ba7886bf48861746c6b86
tim <tim@timschupp.de>
2023-01-04 17:49:57 +0100

    adapted single beat for production

--- back/entry.sh
+++ back/entry.sh
@@ -2,5 +2,10 @@
 # TODO: in the future these procesees should be deomonized or handled by supervisord
 # "rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:DJ_REDIS_PORT"
 celery -A back worker --loglevel=info &
-single-beat celery -A back beat --loglevel=info &
+if [ "$BUILD_TYPE" = "deployment" ]; then
+    echo "rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:$DJ_REDIS_PORT"
+    SINGLE_BEAT_REDIS_SERVER="rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:$DJ_REDIS_PORT" single-beat celery -A back beat --loglevel=info &
+else
+    SINGLE_BEAT_REDIS_SERVER='redis://host.docker.internal:6379' single-beat celery -A back beat --loglevel=info &
+fi
 uvicorn back.asgi:application --reload --port 8000 --host 0.0.0.0
