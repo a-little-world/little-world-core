c77312f268ef6266006fac6cbad79a8ce49e34a1
tim <tim@timschupp.de>
2023-01-04 18:00:46 +0100

    now beat is only started on one node always!

--- back/entry.sh
+++ back/entry.sh
@@ -3,7 +3,6 @@
 # "rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:DJ_REDIS_PORT"
 celery -A back worker --loglevel=info &
 if [ "$BUILD_TYPE" = "deployment" ]; then
-    echo "rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:$DJ_REDIS_PORT"
     SINGLE_BEAT_REDIS_SERVER="rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:$DJ_REDIS_PORT" single-beat celery -A back beat --loglevel=info &
 else
     #SINGLE_BEAT_REDIS_SERVER='redis://host.docker.internal:6379' single-beat celery -A back beat --loglevel=info &
