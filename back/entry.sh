9fe8a7e6db5588ff88c681fbb297c99851d38e06
tim <tim@timschupp.de>
2023-01-04 17:42:27 +0100

    use celery single beat now to try to start only one celery beat worker

--- back/entry.sh
+++ back/entry.sh
@@ -1,5 +1,6 @@
 # This is the general entry point for server startup ( dev )
 # TODO: in the future these procesees should be deomonized or handled by supervisord
+# "rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:DJ_REDIS_PORT"
 celery -A back worker --loglevel=info &
-celery -A back beat --loglevel=info &
+single-beat celery -A back beat --loglevel=info &
 uvicorn back.asgi:application --reload --port 8000 --host 0.0.0.0
