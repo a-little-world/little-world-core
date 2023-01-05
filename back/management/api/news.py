b341710760e37f28f4ea9759e9a73587d6b9d7a4
tim <tim@timschupp.de>
2023-01-05 15:40:01 +0100

    added models and api for news and update elements

---
+++ back/management/api/news.py
@@ -0,0 +1,21 @@
+import datetime
+from rest_framework.views import APIView
+from rest_framework import authentication, permissions, viewsets
+from rest_framework.response import Response
+from django.utils.translation import pgettext_lazy
+from ..models.news_and_updates import NewsItem, NewsItemSerializer
+
+
+def get_all_active_news_items_serialized():
+    active_event = list(NewsItem.get_all_active_news_items())
+    return [NewsItemSerializer(e).data for e in active_event]
+
+
+class GetActiveNewsItemsApi(APIView):
+    authentication_classes = [authentication.SessionAuthentication,
+                              authentication.BasicAuthentication]
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        return Response(get_all_active_news_items_serialized())
---
+++ back/management/models/news_and_updates.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils.translation import pgettext_lazy
+from back.utils import get_options_serializer
+from rest_framework import serializers
+
+
+class NewsItem(models.Model):
+
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.CharField(max_length=255, null=False, blank=False)
+
+    time = models.DateTimeField(null=False, blank=False)
+
+    link = models.CharField(default="", max_length=255)
+
+    active = models.BooleanField(default=False)
+
+    @classmethod
+    def get_all_active_news_items(cls):
+        return cls.objects.all().filter(active=True)
+
+
+class NewsItemSerializer(serializers.ModelSerializer):
+    options = serializers.SerializerMethodField()
+
+    def get_options(self, obj):
+        return get_options_serializer(self, obj)
+
+    class Meta:
+        model = NewsItem
+        fields = ['title', 'description', 'time', 'options', 'link']
