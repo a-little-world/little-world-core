e1c3581241def87e877f7f0061681b67cb681f96
tim <tim@timschupp.de>
2023-05-04 16:31:54 +0200

    linked backend development config into the 'back' init file

--- back/back/__init__.py
+++ back/back/__init__.py
@@ -1,3 +1,10 @@
+"""
+Backend configuration module
+
+Contains standart django configurations in `settings.py` and basic setup of our asgi app in `asgi.py` basic initalization for celery task management in `celery.py`.
+
+.. include:: ./README.md
+"""
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
