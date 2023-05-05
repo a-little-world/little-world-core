d4d2a73ce4e6bedd5e2aeaea427ab093b1298e51
tim <tim@timschupp.de>
2023-05-05 16:44:29 +0200

    back config docs include the docs navigation

--- back/back/__init__.py
+++ back/back/__init__.py
@@ -1,4 +1,7 @@
 """
+.. include:: ../docs_nav.md
+
+
 Backend configuration module
 
 Contains standart django configurations in `settings.py` and basic setup of our asgi app in `asgi.py` basic initalization for celery task management in `celery.py`.
