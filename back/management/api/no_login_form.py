aad54d489b73005b2cdd16426d356901927a2890
tim <tim@timschupp.de>
2023-05-10 14:57:58 +0200

    basic framework for the no login required forms and their submission

---
+++ back/management/api/no_login_form.py
@@ -0,0 +1,72 @@
+"""
+This represents a class of forms that dont require the user to login
+Every form has a unique hash that is always cuppled to a unique user
+That way we can allow to render and updated the form without the user having to log-in
+"""
+from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import api_view, permission_classes, authentication_classes, throttle_classes
+from typing import Literal
+from rest_framework.views import APIView
+from django.views.i18n import JavaScriptCatalog, JSONCatalog
+from django.utils.translation.trans_real import DjangoTranslation
+from django.utils.translation import get_language
+from django.conf import settings
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework_dataclasses.serializers import DataclassSerializer
+from dataclasses import dataclass
+from management.models.no_login_form import NoLoginForm, FORMS
+
+
+@dataclass
+class NoLoginFormData:
+    form_hash: str
+    ford_data: dict
+
+
+@dataclass
+class StillInContactFormData:
+    still_in_contact: bool
+    contact_duration: Literal["1week", "2weeks", "3weeks"]
+
+
+class StillInContactFormDataSerializer(DataclassSerializer):
+    class Meta:
+        dataclass = StillInContactFormData
+
+
+class NoLoginFormDataSerializer(DataclassSerializer):
+
+    class Meta:
+        dataclass = NoLoginFormData
+
+
+@extend_schema(
+    request=NoLoginFormDataSerializer(many=False),
+)
+@api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
+def no_login_form(request):
+    """
+    This api required no authentication but can still link submitted for data to a user 
+    """
+    serializer = NoLoginFormDataSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    data = serializer.save()
+
+    no_login_form = NoLoginForm.objects.get(hash=data.form_hash)
+
+    # now we need to get or create the corresponding form model
+    form_model = FORMS[no_login_form.form_type]["model"]
+    model_object = form_model.objects.filter(
+        form=no_login_form)
+
+    if not model_object.exists():
+        # then we have to first create that form model
+        pass
+
+    # TODO: finish this...
+
+    pass
