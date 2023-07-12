cf40b456b7d91ab2dcf8aa153a24b427b1a32c28
tim <tim@timschupp.de>
2023-07-12 16:24:14 +0200

    updated the script that creates a full db state backup

--- _shell_inject/full_backup_db_state.py
+++ _shell_inject/full_backup_db_state.py
@@ -3,6 +3,7 @@ from back.management import random_test_users  # !dont_include
 from back.management.models import User, Profile  # !dont_include
 from back.management.models import rooms  # !dont_include
 from back.emails import mails  # !dont_include
+import os
 # !include from management.models import rooms
 # !include from management import controller
 # !include from management import random_test_users
@@ -50,17 +51,36 @@ for app in apps.get_app_configs():
 print(f"Total of {apps_to_process} apps to process with a total of {models_to_extract} models to extract.")
 
 models_extracted = 0
+output_dir = "./dumped_data"
+
+output_dir_present = os.path.exists(output_dir)
+if not output_dir_present:
+    os.mkdir(output_dir)
+
+
+models_extracted = 0
+errors = 0
 for app in models_per_app:
     print("Extracting models for app:", app)
+
     for model in models_per_app[app]:
         print("Extracting model:", model)
 
-        with open(f'{model}.json', 'w') as f:
-            with redirect_stdout(f):
-                call_command('dumpdata', '-v', '3', 'contenttypes.ContentType')
-                
-        print("Extracted model:", model, "to file:", f"{model}.json")
-        
-        # Move the file from the mounted container to the host then compress it
-        print("Preparing to compress file: ", f"{model}.json")
-        # TODO: compress then encrypt
+        model_path = f"{output_dir}/{model}"
+
+        try:
+            call_command('dumpdata', '-o', f'{model_path}.json', '-v', '3', '--indent', '2', model)
+                    
+            print("Extracted model_path:", model_path, "to file:", f"{model_path}.json")
+            print("Preparing to compress file: ", f"{model_path}.json")
+            models_extracted += 1
+        except Exception as e:
+            
+            error_msg = f"Error extracting model_path: {model_path} to file: {model_path}.json, error: {e}"
+            
+            with open(f"{model_path}.error", "w") as f:
+                f.write(error_msg)
+
+            print(error_msg)
+            errors += 1
+        print(f"({models_extracted}/{models_to_extract}) [errors: {errors}] finished processing model:", model)
