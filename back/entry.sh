255b7bf0e0d85377384d9a371c4e841a7c77d5fc
tim <tim@timschupp.de>
2023-08-16 07:22:13 +0200

    updated entry

--- back/entry.sh
+++ back/entry.sh
@@ -1,7 +1,12 @@
 # This is the general entry point for server startup ( dev )
 # TODO: in the future these procesees should be deomonized or handled by supervisord
 # "rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:DJ_REDIS_PORT"
-celery -A back worker --loglevel=info &
+if [ "$PROD_LOCAL" = "1" ]; then
+    echo "No celery worker for local prod!"
+else
+    celery -A back worker --loglevel=info &
+fi
+
 if [ "$BUILD_TYPE" = "deployment" ]; then
     SINGLE_BEAT_REDIS_SERVER="rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:$DJ_REDIS_PORT" single-beat celery -A back beat --loglevel=info &
 elif [ "$BUILD_TYPE" = "staging" ]; then
