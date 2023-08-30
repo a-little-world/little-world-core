1c5fd4236415b135efc7a9301b269d4cc9cea0dd
tim <tim@timschupp.de>
2023-08-30 11:27:49 +0200

    always add the matching permission

--- back/management/controller.py
+++ back/management/controller.py
@@ -398,7 +398,7 @@ def create_base_admin_and_add_standart_db_values():
         )
         usr.state.set_user_form_completed()  # Admin doesn't have to fill the userform
         usr.notify("You are the admin master!")
-    #print("BASE ADMIN USER CREATED!")
+    print("BASE ADMIN USER CREATED!")
     
     # Tim Schupp is the new base admin user, we will now create a match with hin instead:
     TIM_MANAGEMENT_USER_MAIL = "tim.timschupp+420@gmail.com"
@@ -412,12 +412,13 @@ def create_base_admin_and_add_standart_db_values():
             first_name="Tim",
             last_name="Schupp",
         )
-        us = usr_tim.state
         # The tim user should always get the matching permission
-        us.extra_user_permissions.append(State.ExtraUserPermissionChoices.MATCHING_USER)
-        us.save()
-        usr_tim.state.set_user_form_completed()  # Admin doesn't have to fill the userform
-        usr_tim.notify("You are the bese management user with less permissions.")
+    usr_tim.state.extra_user_permissions.append(State.ExtraUserPermissionChoices.MATCHING_USER)
+    usr_tim.state.save()
+    usr_tim.state.set_user_form_completed()  # Admin doesn't have to fill the userform
+    usr_tim.notify("You are the bese management user with less permissions.")
+
+    print("TIM ADMIN USER CREATED!")
     
     # Now we create some default database elements that should be part of all setups!
     from management.tasks import (
