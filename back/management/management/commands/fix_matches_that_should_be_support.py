2fb5a7b188c9ff19458e6babeca4e1e5d9eaf05d
tim <tim@timschupp.de>
2023-09-05 12:34:42 +0200

    the right way to add django command execution scripts

---
+++ back/management/management/commands/fix_matches_that_should_be_support.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        from management.models import Match, State
+        from django.db.models import Q
+        
+        # Get all matches where at least one user has matching user permission
+        matchesToUpdate = Match.objects.filter(
+            Q(user1__state__extra_user_permissions__contains=State.ExtraUserPermissionChoices.MATCHING_USER) | 
+            Q(user2__state__extra_user_permissions__contains=State.ExtraUserPermissionChoices.MATCHING_USER)
+        )
+        
+        # Update all matches to support_matching = True in one go
+        matchesToUpdate.update(support_matching=True)
+
+        print(f"Updated {matchesToUpdate.count()} matches.")
