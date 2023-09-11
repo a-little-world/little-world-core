454aa1290fffc9b89e0ddf44063b6d1c030f3b60
tim <tim@timschupp.de>
2023-09-12 01:31:32 +0200

    super simple sms model

---
+++ back/management/models/sms.py
@@ -0,0 +1,30 @@
+from django.db import models
+from rest_framework import serializers
+from .user import User
+from multiselectfield import MultiSelectField
+from uuid import uuid4
+from emails import mails
+
+class SmsModel(models.Model):
+    
+    hash = models.UUIDField(default=uuid4, editable=False)
+
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sms_recipient')
+    send_initator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sms_send_initator')
+    
+    message = models.TextField()
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    twilio_response = models.JSONField(default=dict)
+    
+    @classmethod
+    def send_sms(cls, recipient, send_initator, message):
+        sms = cls.objects.create(
+            recipient=recipient,
+            send_initator=send_initator,
+            message=message
+        )
+        return sms
+    
+    
