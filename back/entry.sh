f9f439e129af2d02d874c2da565b443c504d2b33
tim <tim@timschupp.de>
2023-09-20 19:32:30 +0200

    removed unwanted email translations and added comment about it

--- back/entry.sh
+++ back/entry.sh
@@ -19,6 +19,7 @@ if [ "$EMPHIRIAL" = "1" ]; then
     python3 manage.py migrate --noinput
     python3 manage.py collectstatic --noinput
     # python3 manage.py makemessages -a Only if new translations where added
+    # python3 manage.py makemessages -a --ignore "emails/*" <-- ignore emails, we don't jet offerr translations for them
     python3 manage.py add_tag_translations
     python3 manage.py compilemessages --use-fuzzy
     python3 manage.py shell --command 'from management.controller import create_base_admin_and_add_standart_db_values; create_base_admin_and_add_standart_db_values()'
