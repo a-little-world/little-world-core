7a6260ad200f96b2104ee92f0507aa20dc1753b6
tim <tim@timschupp.de>
2023-11-16 23:43:53 +0100

    prod entry

---
+++ back/entries/entry_prod.sh
@@ -0,0 +1,25 @@
+# This is the general entry point for server startup ( dev )
+# TODO: in the future these procesees should be deomonized or handled by supervisord
+# "rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:DJ_REDIS_PORT"
+
+celery -A back worker --loglevel=info &
+if [ "$BUILD_TYPE" = "deployment" ]; then
+    SINGLE_BEAT_REDIS_SERVER="rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:$DJ_REDIS_PORT" single-beat celery -A back beat --loglevel=info &
+elif [ "$BUILD_TYPE" = "staging" ]; then
+    SINGLE_BEAT_REDIS_SERVER="rediss://:$DJ_REDIS_PASSWORD@$DJ_REDIS_HOST:$DJ_REDIS_PORT" single-beat celery -A back beat --loglevel=info &
+else
+    celery -A back beat --loglevel=info &
+fi
+
+if [ "$BUILD_TYPE" = "deployment" ]; then
+    python3 manage.py migrate --noinput
+fi
+
+if [ "$DJ_USE_WHITENOISE" = "1"]; then
+    python3 manage.py collectstatic --noinput
+fi
+
+python3 manage.py compilemessages --use-fuzzy
+python3 manage.py shell --command 'from management.controller import create_base_admin_and_add_standart_db_values; create_base_admin_and_add_standart_db_values()'
+
+uvicorn back.asgi:application --reload --port 8000 --host 0.0.0.0
