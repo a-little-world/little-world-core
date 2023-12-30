845ad1e23146bb91f249cf12bdea1fdb46194e04
tim <tim@timschupp.de>
2023-12-30 21:09:08 +0100

    capcitor development env

--- front/env_apps/main_frontend.capacitor.env.js
+++ front/env_apps/main_frontend.capacitor.env.js
@@ -1,7 +1,7 @@
 exports.DEVELOPMENT = false; // this is more like FRONTEND_LOCAL_DEVELOPENT ( it's ment to be false, when use in backend local development )
 // Yeah I know this is somewhat redundant, but there is a difference between backend-dev and frontend-dev
 // e.g.: we need to use 'wss' for websocket in production but 'ws' in both frontend and backend localdev
-exports.PRODUCTION = true;
+exports.PRODUCTION = false;
 exports.DEFAULT_LOGIN_USERNAME = this.DEVELOPMENT
   ? 'benjamin.tim@gmx.de'
   : 'nopeHeAintExistInProduction:)';
@@ -12,3 +12,4 @@ exports.BACKEND_URL = 'http://10.0.2.2:8000';
 exports.BACKEND_PATH = '/app';
 exports.CORE_WS_SHEME = 'ws://';
 exports.CORE_WS_PATH = '/api/core/ws';
+exports.IS_CAPACITOR_BUILD = true;
