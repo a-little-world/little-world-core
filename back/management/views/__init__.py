f35949d089da23aee0501fd762ff5aea0b10086d
tim <tim@timschupp.de>
2024-01-09 19:06:39 +0100

    register info card view

--- back/management/views/__init__.py
+++ back/management/views/__init__.py
@@ -1,2 +1,2 @@
-from .main_frontend import MainFrontendView, PublicMainFrontendView
+from .main_frontend import MainFrontendView, PublicMainFrontendView, info_card
 from .landing_page import landing_page
