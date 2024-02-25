bd94d80cad49f6cd70767f279e2baeeb07029f3a
Tim Schupp <tim@timschupp.de>
2024-02-25 21:18:35 +0100

    pass full updated user data when profile is completed

--- back/management/api/user_data.py
+++ back/management/api/user_data.py
@@ -349,7 +349,53 @@ def serialize_notifications(notifications):
 
     return serialized
 
+def user_data(user):
+    user_state = user.state
+    user_profile = user.profile
+    
+    is_matching_user = user_state.has_extra_user_permission(State.ExtraUserPermissionChoices.MATCHING_USER)
 
+    ProfileWOptions = transform_add_options_serializer(SelfProfileSerializer)
+    profile_data = ProfileWOptions(user_profile).data
+    del profile_data["options"]
+
+    
+    support_matches = get_paginated(Match.get_support_matches(user), 10, 1)
+    support_matches["items"] = serialize_matches(support_matches["items"], user)
+    
+    
+    cal_data_link = "{calcom_meeting_id}?{encoded_params}".format(first_name=user.profile.first_name,encoded_params=urllib.parse.urlencode({
+                        "email": str(user.email),
+                        "hash": str(user.hash),
+                        "bookingcode": str(user.state.prematch_booking_code)
+    }), calcom_meeting_id=settings.DJ_CALCOM_MEETING_ID)
+    
+    
+    pre_match_appointent = PreMatchingAppointment.objects.filter(user=user).order_by("created")
+    if pre_match_appointent.exists():
+        pre_match_appointent = PreMatchingAppointmentSerializer(pre_match_appointent.first()).data
+    else:
+        pre_match_appointent = None
+
+    # Prematching join link depends on the support user
+    pre_call_join_link = None
+    if len(support_matches['items']) > 0:
+        pre_call_join_link = f"/app/call-setup/{support_matches['items'][0]['partner']['id']}/"
+    
+    return {
+            "id": user.hash,
+            "status": FrontendStatusSerializer(user_state).data["status"],
+            "isSupport": is_matching_user,
+            "isSearching": user_state.matching_state == State.MatchingStateChoices.SEARCHING,
+            "email": user.email,
+            "preMatchingAppointment": pre_match_appointent,
+            'preMatchingCallJoinLink': pre_call_join_link,
+            "calComAppointmentLink": cal_data_link,
+            "hadPreMatchingCall": user_state.had_prematching_call,
+            "emailVerified": user_state.email_authenticated,
+            "userFormCompleted": user_state.user_form_state == State.UserFormStateChoices.FILLED, # TODO: depricate
+            "profile": profile_data,
+    }
 
 def frontend_data(user, items_per_page=10, request=None):
 
@@ -382,58 +428,22 @@ def frontend_data(user, items_per_page=10, request=None):
     archived_notifications = get_paginated(Notification.get_archived_notifications(user), items_per_page, 1)
     archived_notifications["items"] = serialize_notifications(archived_notifications["items"])
 
-    ProfileWOptions = transform_add_options_serializer(SelfProfileSerializer)
-    profile_data = ProfileWOptions(user_profile).data
-    profile_options = profile_data["options"]
-    del profile_data["options"]
-
-    # TODO: Currently incoming calls are only populated if the user was online when the incomin call was triggered
-    # This should be refactored so that we can actually tracka nd display whena user is currently in a call
-    
-    
-    #chats = ChatSerializer(Paginator(Chat.get_chats(user), items_per_page).page(1), many=True).data
-    
-    
     empty_list = {
         "items": [],
         "totalItems": 0,
         "itemsPerPage": 0,
         "currentPage": 0,
     }
-    
-    cal_data_link = "{calcom_meeting_id}?{encoded_params}".format(first_name=user.profile.first_name,encoded_params=urllib.parse.urlencode({
-                        "email": str(user.email),
-                        "hash": str(user.hash),
-                        "bookingcode": str(user.state.prematch_booking_code)
-    }), calcom_meeting_id=settings.DJ_CALCOM_MEETING_ID)
-    
-    
-    pre_match_appointent = PreMatchingAppointment.objects.filter(user=user).order_by("created")
-    if pre_match_appointent.exists():
-        pre_match_appointent = PreMatchingAppointmentSerializer(pre_match_appointent.first()).data
-    else:
-        pre_match_appointent = None
 
-    # Prematching join link depends on the support user
-    pre_call_join_link = None
-    if len(support_matches['items']) > 0:
-        pre_call_join_link = f"/app/call-setup/{support_matches['items'][0]['partner']['id']}/"
+    ProfileWOptions = transform_add_options_serializer(SelfProfileSerializer)
+    profile_data = ProfileWOptions(user_profile).data
+
+    profile_options = profile_data["options"]
+    
+    ud = user_data(user)
 
     frontend_data = {
-        "user": {
-            "id": user.hash,
-            "status": FrontendStatusSerializer(user_state).data["status"],
-            "isSupport": is_matching_user,
-            "isSearching": user_state.matching_state == State.MatchingStateChoices.SEARCHING,
-            "email": user.email,
-            "preMatchingAppointment": pre_match_appointent,
-            'preMatchingCallJoinLink': pre_call_join_link,
-            "calComAppointmentLink": cal_data_link,
-            "hadPreMatchingCall": user_state.had_prematching_call,
-            "emailVerified": user_state.email_authenticated,
-            "userFormCompleted": user_state.user_form_state == State.UserFormStateChoices.FILLED, # TODO: depricate
-            "profile": profile_data,
-        },
+        "user": ud,
         "communityEvents": community_events,
         "matches": {
             # Switch case here cause for support users all matches are 'support' matches :D
