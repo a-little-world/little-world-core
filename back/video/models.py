0555e311bce1e3a98abe014d28b91a3860b0573a
Tim Schupp <tim@timschupp.de>
2024-03-21 14:46:31 +0100

    new 'video' django module, also added inital implementation of DB models fore LiveKitRoom & LiveKitSession ( for tracking live room times etc )

---
+++ back/video/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.db.models import Q
+from uuid import uuid4
+
+class LiveKitRoom(models.Model):
+    
+    uuid = models.UUIDField(default=uuid4, editable=False, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    u1 = models.ForeignKey("management.User", on_delete=models.CASCADE, related_name="u1_livekit_room")
+    u2 = models.ForeignKey("management.User", on_delete=models.CASCADE, related_name="u2_livekit_room")
+    
+
+class LivekitSession(models.Model):
+    
+    uuid = models.UUIDField(default=uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    
+    u1 = models.ForeignKey("management.User", on_delete=models.CASCADE, related_name="u1_livekit_session")
+    u2 = models.ForeignKey("management.User", on_delete=models.CASCADE, related_name="u2_livekit_session")
+    
+    events = models.JSONField(default=list)
+    
