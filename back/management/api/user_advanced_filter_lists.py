3161c72d2d51bd7ff6b0e7e9648709817f0edd61
Tim Schupp <tim@timschupp.de>
2024-06-04 01:31:25 +0200

    depricate old user journey, add new filters

--- back/management/api/user_advanced_filter_lists.py
+++ back/management/api/user_advanced_filter_lists.py
@@ -14,6 +14,24 @@ from management.api.user_advanced_filter import (
     users_with_booked_prematching_call,
     users_require_prematching_call_not_booked
 )
+from management.api.user_journey_filters import (
+    user_created,
+    email_verified,
+    user_form_completed,
+    booked_onboarding_call,
+    first_search,
+    user_searching,
+    pre_matching,
+    match_takeoff,
+    never_active,
+    no_show,
+    ghoster,
+    no_confirm,
+    happy_inactive,
+    too_low_german_level,
+    unmatched,
+    gave_up_searching
+)
 
 @dataclass
 class FilterListEntry:
@@ -98,4 +116,84 @@ FILTER_LISTS = [
         "Users that have booked a pre-matching call but have not had one yet",
         users_with_booked_prematching_call
     ),
+    FilterListEntry(
+        "ujv2_user_created",
+        "User Journey V2: User was created, but still has to verify mail, fill form and have a prematching call",
+        user_created
+    ),
+    FilterListEntry(
+        "ujv2_email_verified",
+        "User Journey V2: User has verified email, but still has to fill form and have a prematching call",
+        email_verified
+    ),
+    FilterListEntry(
+        "ujv2_user_form_completed",
+        "User Journey V2: User has filled form, but still has to have a prematching call",
+        user_form_completed
+    ),
+    FilterListEntry(
+        "ujv2_booked_onboarding_call",
+        "User Journey V2: User has filled form and booked onboarding call",
+        booked_onboarding_call
+    ),
+    FilterListEntry(
+        "ujv2_first_search",
+        "User Journey V2: User is doing first search i.e.: has no 'non-support' match",
+        first_search
+    ),
+    FilterListEntry(
+        "ujv2_user_searching",
+        "User Journey V2: User is searching and has at least one match",
+        user_searching
+    ),
+    FilterListEntry(
+        "ujv2_pre_matching",
+        "User Journey V2: User has `Pre-Matching` or `Kickoff-Matching` Match.",
+        pre_matching
+    ),
+    FilterListEntry(
+        "ujv2_match_takeoff",
+        "User Journey V2: User has `Pre-Matching` or `Kickoff-Matching` Match.",
+        match_takeoff
+    ),
+    FilterListEntry(
+        "ujv2_never_active",
+        "User Journey V2: Didn't ever become active",
+        never_active
+    ),
+    FilterListEntry(
+        "ujv2_no_show",
+        "User Journey V2: Didn't show up to onboarding call",
+        no_show
+    ),
+    FilterListEntry(
+        "ujv2_ghoster",
+        "User Journey V2: User has matching in [3.G] 'ghosted' his match",
+        ghoster
+    ),
+    FilterListEntry(
+        "ujv2_no_confirm",
+        "User Journey V2: Learner that has matching in 'Never Confirmed'",
+        no_confirm
+    ),
+    FilterListEntry(
+        "ujv2_happy_inactive",
+        "User Journey V2: Not searching, 1 or more matches at least one match in 'Completed Matching'",
+        happy_inactive
+    ),
+    FilterListEntry(
+        "ujv2_too_low_german_level",
+        "User Journey V2: User never active, but was flagged with a 'state.to_low_german_level=True'",
+        too_low_german_level
+    ),
+    FilterListEntry(
+        "ujv2_unmatched",
+        "User Journey V2: 'first-search' for over XX days, we failed to match the user at all",
+        unmatched
+    ),
+    FilterListEntry(
+        "ujv2_gave_up_searching",
+        "User Journey V2: User that's `searching=False` and has 0 matches",
+        gave_up_searching
+    )
 ]
