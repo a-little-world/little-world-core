5ca6842b0c615c4da6f9b14689b02526f9c9148a
Jakob Elias Gebler <jakob.gebler@rwth-aachen.de>
2024-06-18 13:26:58 +0200

    remove regex escaping because of limiting user exp

--- back/management/api/help.py
+++ back/management/api/help.py
@@ -1,20 +1,10 @@
-from rest_framework import viewsets, authentication, permissions
-import io
-import re
-from management.models.profile import SelfProfileSerializer, Profile
+from rest_framework import authentication, permissions
 from rest_framework.response import Response
-from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
+from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
-from rest_framework import serializers, status
-from dataclasses import dataclass
-from back.utils import transform_add_options_serializer
+from rest_framework import serializers
 from management.models.help_message import HelpMessage
 from django.core.validators import MaxLengthValidator, MinLengthValidator
-
-
-def validate_message_characters(value):
-    corrected_value = re.sub(r"[^a-zA-Z0-9\s,.:!?\-]", "_", value)
-    return corrected_value
 
 
 class SendHelpMessageSerializer(serializers.Serializer):
@@ -23,9 +13,6 @@ class SendHelpMessageSerializer(serializers.Serializer):
         validators=[MinLengthValidator(3), MaxLengthValidator(2000)],
     )
     file = serializers.ListField(child=serializers.FileField(), required=False)
-
-    def validate_message(self, value):
-        return validate_message_characters(value)
 
     def create(self, validated_data):
         return validated_data
