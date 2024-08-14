f266799ced1747405c64de1aa34df2d5a81b95d8
Tim Schupp <tim@timschupp.de>
2024-08-14 17:42:19 +0200

    helpers to exteralize commonly used functions and classes

---
+++ back/management/helpers/__init__.py
@@ -0,0 +1,5 @@
+from .is_admin_or_matching_user import IsAdminOrMatchingUser
+
+__all__ = [
+    'IsAdminOrMatchingUser',
+]
---
+++ back/management/helpers/is_admin_or_matching_user.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdminOrMatchingUser(BasePermission):
+    """
+    Allows access only to admin users.
+    """
+
+    def has_permission(self, request, view):
+        from management.models.state import State
+        return bool(request.user and request.user.is_staff) or \
+            bool(request.user and request.user.is_authenticated and request.user.state.has_extra_user_permission(
+                State.ExtraUserPermissionChoices.MATCHING_USER))
