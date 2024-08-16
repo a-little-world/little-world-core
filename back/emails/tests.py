530498039ca09311b6c51b1ba9ed0acc89c91267
Tim Schupp <tim@timschupp.de>
2024-08-16 13:09:07 +0200

    new currently failing complete email test run

--- back/emails/tests.py
+++ back/emails/tests.py
@@ -1,20 +1,25 @@
 from django.test import TestCase
+from management.tests.helpers import register_user
+from emails.api_v2.emails_config import EMAILS_CONFIG, EmailsConfig
+from emails.api_v2.render_template import prepare_template_context, render_template_to_html, get_full_template_info
+from management.controller import match_users
 
 
 class EmailTests(TestCase):
 
-    def test_send_email_dev(self):
-        """
-        test sending and email without sending and email
-        by just generating the html for the template from dummy data
-        """
-        pass  # TODO
-
-    def test_send_email_staging_test(self):
-        # TODO: In the future this should be a *real* send email test
-        # This should use safely modified stage_env params to send email to and test account
-        # Then we should use some credentials to fetch that test email and see im e.g.: the registration code is correct
-        pass
-
-    def test_logging_email(self):
-        pass  # TODO: test the creation of the 'email' model
+    def test_send_all_emails(self):
+        # Test sending all emails
+        u1 = register_user()
+        u2 = register_user()
+        
+        match = match_users({
+            u1, u2
+        })
+        
+        for template_name in EMAILS_CONFIG.emails:
+            print("Sending Email '{}'".format(template_name))
+            
+            template_info, context = prepare_template_context(template_name, u1.id, match.id)
+            
+            # We should check all the used template variables and if they are correctly injected
+            email_html = render_template_to_html(template_info['config']['template'], context)
