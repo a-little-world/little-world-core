b2c700b5c0764a0373196a30beeed013deb1abc0
Tim Schupp <tim@timschupp.de>
2024-10-31 14:41:56 +0100

    updated email settings api

--- back/emails/api/email_settings.py
+++ back/emails/api/email_settings.py
@@ -16,6 +16,11 @@ from management.controller import get_base_management_user
 from emails.models import EmailLog
 from management.models.settings import EmailSettings
 from emails.api.emails_config import EMAILS_CONFIG
+from rest_framework import serializers
+
+def get_unsubscribed_categories():
+    unsubscribale_categories = [category for category in EMAILS_CONFIG.categories if EMAILS_CONFIG.categories[category].unsubscribe]
+    return unsubscribale_categories
 
 @api_view(["GET"])
 @permission_classes([])
@@ -28,14 +33,55 @@ def retrieve_email_settings(request, email_settings_hash):
     
     settings = settings.first()
     
-    unsubscribale_categories = [category for category in EMAILS_CONFIG.categories if EMAILS_CONFIG.categories[category].unsubscribe]
+    unsubscribale_categories = get_unsubscribed_categories()
+    subscribed_categories = [category for category in unsubscribale_categories if (category not in settings.unsubscribed_categories)]
     
     return Response({
         "categories": unsubscribale_categories,
-        "unsubscribed_categories": settings.unsubscribed_categories
+        "unsubscribed_categories": settings.unsubscribed_categories,
+        "subscribed_categories": subscribed_categories
     })
     
+def toggle_category_subscribe(request, email_settings_hash, category, subscribed=False):
+    settings = EmailSettings.objects.filter(hash=email_settings_hash) 
+    if not settings.exists():
+        return Response({"detail": "Not found"}, status=404)
+    
+    settings = settings.first()
+    
+    unsubscribale_categories = get_unsubscribed_categories()
+    if category not in unsubscribale_categories:
+        return Response({"detail": "Invalid category"}, status=400)
+    
+    if not subscribed:
+        # Unsubscribe
+        if category in settings.unsubscribed_categories:
+            return Response({"detail": "Already unsubscribed"}, status=400)
+        else:
+            settings.unsubscribed_categories.append(category)
+            return Response({"detail": f"Ubscubscribed '{category}'"})
+    else:
+        # Subscribe
+        if category not in settings.unsubscribed_categories:
+            return Response({"detail": "Already subscribed"}, status=400)
+        else:
+            settings.unsubscribed_categories.remove(category)
+            return Response({"detail": f"Subscribed '{category}'"})
+        
+@api_view(["POST"])
+@permission_classes([])
+@authentication_classes([])
+def unscubscribe_category(request, email_settings_hash, category):
+    return toggle_category_subscribe(request, email_settings_hash, category, subscribed=False)
 
+@api_view(["POST"])
+@permission_classes([])
+@authentication_classes([])
+def subscribe_category(request, email_settings_hash, category):
+    return toggle_category_subscribe(request, email_settings_hash, category, subscribed=True)
+    
 api_urls = [
     path("api/email_settings/<str:email_settings_hash>/", retrieve_email_settings),
+    path("api/email_settings/<str:email_settings_hash>/<str:category>/subscribe", unscubscribe_category),
+    path("api/email_settings/<str:email_settings_hash>/<str:category>/unsubscribe", subscribe_category),
 ]
