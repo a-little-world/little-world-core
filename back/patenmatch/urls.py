095f803339bab713b106aa30eabb137c0f733908
Jakob Gebler <jakobgebler@mail.de>
2024-11-12 14:59:19 +0100

    remove old paginator class commands

--- back/patenmatch/urls.py
+++ back/patenmatch/urls.py
@@ -47,14 +47,6 @@ class PatenmatchUserViewSet(viewsets.ModelViewSet):
 
 
 class PatenmatchOrganizationViewSet(viewsets.ModelViewSet):
-    # class CustomPagination(PageNumberPagination):
-    #    page_size = 10
-    #    page_size_query_param = "page_size"
-    #    max_page_size = 100
-    #
-    #    def get_paginated_response(self, data):
-    #        return response.Response({"count": self.page.paginator.count, "total_pages": self.page.paginator.num_pages, "current_page": self.page.number, "next": self.get_next_link(), "previous": self.get_previous_link(), "results": data})
-
     queryset = PatenmatchOrganization.objects.all()
     serializer_class = PatenmatchOrganizationSerializer
     pagination_class = DetailedPagination
