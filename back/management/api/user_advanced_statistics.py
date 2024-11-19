3ca37c7ab63dd6166065c81231d5c654fba87fed
Tim Schupp <tim@timschupp.de>
2024-11-19 15:46:18 +0100

    cummulative waiting times also add 'count_vol' so we can see the portion of volunteers, also small comment to be read when the time comes

--- back/management/api/user_advanced_statistics.py
+++ back/management/api/user_advanced_statistics.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes
 from management.models.user import User
+from management.models.profile import Profile
+from management.models.state import State
 from video.models import LivekitSession
 from management.models.matches import Match
 from django.db.models.functions import TruncDay, TruncWeek, TruncMonth
@@ -62,17 +64,31 @@ def user_signups(request):
 
     # Calculate the count of users who joined before the start_date
     pre_start_date_count = queryset.filter(date_joined__lt=start_date).count()
+    pre_start_data_count_volunteer = queryset.filter(date_joined__lt=start_date, profile__user_type=Profile.TypeChoices.VOLUNTEER).count()
 
     user_counts = queryset.filter(date_joined__range=[start_date, end_date]).annotate(bucket=trunc_func("date_joined")).values("bucket").annotate(count=Count("id")).order_by("bucket")
+    volunteer_only_users = queryset.filter(date_joined__range=[start_date, end_date], profile__user_type=Profile.TypeChoices.VOLUNTEER).annotate(bucket=trunc_func("date_joined")).values("bucket").annotate(count=Count("id")).order_by("bucket")
 
     if cumulative:
         cumulative_count = pre_start_date_count
+        cumulative_count_volunteer = pre_start_data_count_volunteer
         data = []
-        for stats in user_counts:
+        for i, stats in enumerate(user_counts):
             cumulative_count += stats["count"]
-            data.append({"date": stats["bucket"], "count": cumulative_count})
+            cumulative_count_volunteer += volunteer_only_users[i]["count"]
+            data.append({
+                "date": stats["bucket"], 
+                "count": cumulative_count,
+                "count_vol": cumulative_count_volunteer
+            })
     else:
-        data = [{"date": stats["bucket"], "count": stats["count"]} for stats in user_counts]
+        data = []
+        for i, stats in enumerate(user_counts):
+            data.append({
+                "date": stats["bucket"], 
+                "count": stats["count"],
+                "count_vol": volunteer_only_users[i]["count"]
+            })
 
     return Response(data)
 
@@ -555,6 +571,10 @@ def user_match_waiting_time_statistics(request):
     num_users = 0
 
     for user in eligible_users:
+        # @Simba14 comment for the next time you look at this code:
+        # It's not super easy to call methods from another class 
+        # ( I think you are trying to call user_advanced.AvdancedUserViewset.match_waiting_time )
+        # Rather I'd recommend to make a helper match_waiting_time(user) and use that here and in user_advanced.AvdancedUserViewset.match_waiting_time
         waiting_time = self.match_waiting_time(request, pk=user.pk).data.get("waiting_time")
         if waiting_time is not None:
             total_waiting_time += waiting_time
