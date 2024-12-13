2dd8d059e81388573a5687ff39849b2103d9c3f9
Tim Schupp <tim@timschupp.de>
2024-12-13 18:03:36 +0100

    update field supplied in the patenmatch admin for each user, to make debugging easier

--- back/patenmatch/admin.py
+++ back/patenmatch/admin.py
@@ -4,12 +4,19 @@ from patenmatch.models import PatenmatchUser, PatenmatchOrganization
 
 @admin.register(PatenmatchUser)
 class PatenmatchUserAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "postal_code", "email", "support_for")
+    list_display = ("first_name", "last_name", "postal_code", "email", "support_for", "uuid", "get_email_verification_link", "get_user_status_url")
 
     search_fields = (
+        "uuid",
         "email",
         "postal_code",
     )
+    
+    def get_email_verification_link(self, obj):
+        return obj.get_verification_url()
+    
+    def get_user_status_url(self, obj):
+        return f"/en/user/{obj.id}/status?status_access_token={obj.status_access_token}"
 
 
 @admin.register(PatenmatchOrganization)
