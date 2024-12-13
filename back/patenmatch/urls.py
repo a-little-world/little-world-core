ccf8bdf7c2c9df4c92c05eeb33dd2668c2726ee6
Tim Schupp <tim@timschupp.de>
2024-12-13 18:03:03 +0100

    register email verification path

--- back/patenmatch/urls.py
+++ back/patenmatch/urls.py
@@ -8,6 +8,7 @@ router.register(r"organization", PatenmatchOrganizationViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     # Expplictly expose user apis, as otherwise with "get" in http_method_names it will try to also register user/<pk/retrieve
+    path("user/verify_email/", PatenmatchUserViewSet.as_view({"get": "verify_email"})),
     path("user/", PatenmatchUserViewSet.as_view({"post": "create"})),
     path("user/<int:pk>/status/", PatenmatchUserViewSet.as_view({"get": "status"})),
 ]
