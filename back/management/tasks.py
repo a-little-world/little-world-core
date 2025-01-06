94e1a2eae3377f3bd66a2cbe86590984027f830b
Tim Schupp <tim@timschupp.de>
2025-01-06 14:52:49 +0100

    improved match expired 'unresponsive' marking logic, and made it log it's changes to the user notes

--- back/management/tasks.py
+++ back/management/tasks.py
@@ -175,9 +175,11 @@ def check_prematch_email_reminders_and_expirations():
     for unclosed in all_unclosed_unconfirmed:
         if unclosed.is_expired(close_if_expired=True, send_mail_if_expired=True):
             # Now we have to set the learner to unresponsive = True and to searching = IDLE
-            unclosed.learner_when_created.state.searching_state = State.SearchingStateChoices.IDLE
-            unclosed.learner_when_created.state.unresponsive = True
-            unclosed.learner_when_created.state.save()
+            learner_state = unclosed.learner_when_created.state
+            learner_state.searching_state = State.SearchingStateChoices.IDLE
+            learner_state.unresponsive = True
+            learner_state.append_notes(f"Set to unresponsive cause let proposal expire: 'proposal:{unclosed.pk}'")
+            learner_state.save()
             continue
         unclosed.is_reminder_due(send_reminder=True)
 
