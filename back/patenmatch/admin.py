bf5a6960689fa89c024a9dcf89b58a35439e9096
Tim Schupp <tim@timschupp.de>
2025-01-07 15:38:51 +0100

    more detailed patenmatch overviews

--- back/patenmatch/admin.py
+++ back/patenmatch/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from patenmatch.models import PatenmatchUser, PatenmatchOrganization
+from patenmatch.models import PatenmatchUser, PatenmatchOrganization, PatenmatchOrganizationUserMatching
 
 
 @admin.register(PatenmatchUser)
 class PatenmatchUserAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "postal_code", "email", "support_for", "uuid", "get_email_verification_link", "get_user_status_url", "get_verification_view_url")
+    list_display = ("first_name", "last_name", "postal_code", "email", "support_for", "uuid", "get_email_verification_link", "get_user_status_url", "get_verification_view_url", "list_matched_organizations")
 
     search_fields = (
         "uuid",
@@ -18,13 +18,23 @@ class PatenmatchUserAdmin(admin.ModelAdmin):
     def get_verification_view_url(self, obj):
         return obj.get_verification_view_url()
     
+    def list_matched_organizations(self, obj):
+        org = PatenmatchOrganizationUserMatching.objects.filter(user=obj)
+
+        if org.exists():
+            return f"Matched to {org.first().organization.name}"
+        return "No organization matched"
+    
     def get_user_status_url(self, obj):
         return f"/en/user/{obj.id}/status?status_access_token={obj.status_access_token}"
 
+@admin.register(PatenmatchOrganizationUserMatching)
+class PatenmatchOrganizationUserMatchingAdmin(admin.ModelAdmin):
+    list_display = ("organization", "user", "created_at", "match_user_name", "match_organization_name", "match_organization_email")
 
 @admin.register(PatenmatchOrganization)
 class PatenmatchOrganizationAdmin(admin.ModelAdmin):
-    list_display = ("name", "postal_code", "contact_email")
+    list_display = ("name", "postal_code", "contact_email", "list_matched_users")
 
     search_fields = (
         "name",
