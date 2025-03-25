cc2003e8663c841afd3736cf33c32d132d1eaf39
Tim Schupp <tim@timschupp.de>
2025-03-25 15:07:02 +0100

    added another more descriptive filter 'learners_above_a1a2'

--- back/management/models/custom_banner_event_filters.py
+++ back/management/models/custom_banner_event_filters.py
@@ -3,11 +3,12 @@ from management.models.profile import Profile
 
 class CustomFilterChoices(models.TextChoices):
     CAPEGEMINI = "capegemini", "capegemini"
+    LEARNERS_ABOVE_A1A2 = "learners_above_a1a2", "learners_above_a1a2"
     VOLUNTEERS = "volunteers", "volunteers"
     LEARNERS = "learners", "learners"
     NONE = "none", "None"
 
-def filter__capegemini(user):
+def filter__learners_above_a1a2(user):
 
     lang_skill_german = list(filter(lambda x: x["lang"] == "german", user.profile.lang_skill))
     german_level = lang_skill_german[0]["level"] if len(lang_skill_german) > 0 else Profile.LanguageSkillChoices.LEVEL_0
@@ -23,7 +24,8 @@ def filter__learners(user):
 
 
 FILTER_FUNC_MAP = {
-    CustomFilterChoices.CAPEGEMINI: filter__capegemini,
+    CustomFilterChoices.CAPEGEMINI: filter__learners_above_a1a2,
+    CustomFilterChoices.LEARNERS_ABOVE_A1A2: filter__learners_above_a1a2,
     CustomFilterChoices.VOLUNTEERS: filter__volunteers,
     CustomFilterChoices.LEARNERS: filter__learners,
 }
