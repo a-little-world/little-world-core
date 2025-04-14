2c5aea858db72bc34cd8adfb5d2f41d723e0b0f1
Tim Schupp <tim@timschupp.de>
2025-04-14 23:50:34 +0200

    pre-rendered still in contact in active email

--- back/emails/api/template_lookup.py
+++ back/emails/api/template_lookup.py
@@ -145,3 +145,21 @@ def prematching_booking_link(user):
         ),
         calcom_meeting_id=settings.DJ_CALCOM_MEETING_ID,
     )
+
+
+def get_still_in_contact_yes_url(user_id, match_id, **kwargs):
+    """
+    Generate URL for confirming continued contact with match partner outside the platform
+    """
+    base_url = settings.BASE_URL
+    token = "TODO" # TODO generate_token_for_user(user_id)
+    return f"{base_url}/still-in-contact/yes/{match_id}?token={token}"
+
+
+def get_still_in_contact_no_url(user_id, match_id, **kwargs):
+    """
+    Generate URL for indicating no continued contact with match partner
+    """
+    base_url = settings.BASE_URL
+    token = "TODO" # TODO generate_token_for_user(user_id)
+    return f"{base_url}/still-in-contact/no/{match_id}?token={token}"
