c6e51bfbc5b90bedc27be2f042a891a2e9d13c04
JannisToelle <github.z8zet@simplelogin.com>
2025-10-21 12:57:52 +0200

    formatting

--- back/management/api/native_auth.py
+++ back/management/api/native_auth.py
@@ -1,52 +1,22 @@
+from typing import Any
 
-import hashlib
-import hmac
-import secrets
-import time
-import urllib.parse
-from dataclasses import dataclass
-from typing import Optional
-from django.urls import path
-from management.api.user import get_user_data
 from django.conf import settings
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.core.cache import cache
-from django.db.models import Q
-from django.dispatch import receiver
-from django.http import HttpResponseRedirect
-from django.shortcuts import redirect
-from django.urls import reverse
-from django_rest_passwordreset.signals import reset_password_token_created
-from drf_spectacular.utils import OpenApiParameter, extend_schema, inline_serializer
-from emails import mails
-from emails.mails import PwResetMailParams, get_mail_data_by_name
-from rest_framework import authentication, permissions, serializers, status
-from rest_framework.authentication import SessionAuthentication
+from django.contrib.auth import authenticate
+from django.urls import path
+from rest_framework import serializers, status
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from tracking import utils
-from tracking.models import Event
-from translations import get_translation
-
-from management.controller import UserNotFoundErr, delete_user, get_user, get_user_by_email, get_user_by_hash
-from management.authentication import NativeOnlyJWTAuthentication as JWTAuthentication
-from django.db.models import Q
-from typing import Any, Dict
-
-from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework_simplejwt.tokens import RefreshToken, UntypedToken
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from translations import get_translation
 
 from management.api.app_integrity import _verify_play_integrity_token
+from management.api.user import get_user_data
 
 # --------------- Native Login API ------------------
+
 
 class NativeAuthApiSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
@@ -67,33 +37,35 @@ def native_auth(request):
     integrity_token = serializer.validated_data["integrity_token"]
     request_hash = serializer.validated_data["request_hash"]
 
-    bypass_integrity_check = settings.NATIVE_APP_INTEGRITY_ALLOW_BYPASS and (integrity_token == settings.NATIVE_APP_INTEGRITY_BYPASS_TOKEN)
+    bypass_integrity_check = settings.NATIVE_APP_INTEGRITY_ALLOW_BYPASS and (
+        integrity_token == settings.NATIVE_APP_INTEGRITY_BYPASS_TOKEN
+    )
     if not bypass_integrity_check and not _verify_play_integrity_token(integrity_token, request_hash):
         return Response({"detail": "Invalid integrity token or request hash"}, status=status.HTTP_400_BAD_REQUEST)
 
     usr = authenticate(username=email.lower(), password=password)
     if usr is None:
         return Response(get_translation("api.login_failed"), status=status.HTTP_400_BAD_REQUEST)
-    
+
     if usr.is_staff:
         return Response(get_translation("api.login_failed_staff"), status=status.HTTP_400_BAD_REQUEST)
 
     from rest_framework_simplejwt.tokens import RefreshToken
+
     refresh = RefreshToken.for_user(usr)
     refresh["client"] = "native"
 
-    return Response({
-        "token_access": str(refresh.access_token),
-        "token_refresh": str(refresh),
-        **get_user_data(usr)
-    })
+    return Response({"token_access": str(refresh.access_token), "token_refresh": str(refresh), **get_user_data(usr)})
+
 
 # --------------- Native Token Refresh API -----------------
+
 
 class NativeTokenRefreshView(TokenRefreshView):
     """
     Allows to re-fesh native token but only if integrity challenged again
     """
+
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:  # type: ignore[override]
         refresh_raw: str | None = request.data.get("refresh")  # type: ignore[assignment]
         if not refresh_raw:
@@ -103,21 +75,24 @@ class NativeTokenRefreshView(TokenRefreshView):
             refresh = RefreshToken(refresh_raw)
         except TokenError as exc:  # includes ExpiredSignatureError etc
             raise InvalidToken(str(exc))
-        
+
         integrity_token = request.data.get("integrity_token")
         request_hash = request.data.get("request_hash")
         if not integrity_token or not request_hash:
             return Response({"detail": "Missing integrity token or request hash"}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         if not _verify_play_integrity_token(integrity_token, request_hash):
             return Response({"detail": "Invalid integrity token or request hash"}, status=status.HTTP_400_BAD_REQUEST)
 
         client = refresh.get("client")
         if client != "native":
-            return Response({"detail": "Refresh token not valid for native client"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {"detail": "Refresh token not valid for native client"}, status=status.HTTP_401_UNAUTHORIZED
+            )
 
         response = super().post(request, *args, **kwargs)
         return response
+
 
 class NativeTokenVerifyView(TokenVerifyView):
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:  # type: ignore[override]
@@ -141,7 +116,7 @@ class NativeTokenVerifyView(TokenVerifyView):
 
         if not _verify_play_integrity_token(integrity_token, request_hash):
             return Response({"detail": "Invalid integrity token or request hash"}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         return super().post(request, *args, **kwargs)
 
 
