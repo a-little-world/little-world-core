20b1ca76c4b5949b17a9114bc8d040734ec0b816
Tim Schupp <tim@timschupp.de>
2025-11-11 12:39:37 +0100

    removed depricated V1 email apis

--- back/emails/urls.py
+++ back/emails/urls.py
@@ -1,21 +1,9 @@
 from django.urls import path
 from rest_framework import routers
 
-from emails import depricated_api
 from emails.api import backend_templates, dev_update_backend_emails, dynamic_template, email_settings, send_email
 
-router = routers.SimpleRouter()
-router.register("api/admin/email/logs", depricated_api.EmailListView)
-
-
-api_routes = [
-    path("api/admin/email/templates/", depricated_api.ListEmailTemplates.as_view()),
-    path("api/admin/email/templates/encode/", depricated_api.EncodeTemplate.as_view()),
-    *router.urls,
-]
-
 urlpatterns = [
-    *api_routes,
     *backend_templates.api_urls,
     *dev_update_backend_emails.api_urls,
     *send_email.api_urls,
