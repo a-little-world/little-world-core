94af327113984e7760f867ae84dfae174999deaf
Tim Schupp <tim@timschupp.de>
2025-11-17 17:50:27 +0100

    update asgi.py to fix import order to not break django init

--- back/back/asgi.py
+++ back/back/asgi.py
@@ -5,10 +5,12 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 
-from back.channels_auth import MultiAuthMiddlewareStack
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "back.settings")
 django_asgi_app = get_asgi_application()
+
+# Import after Django is initialized to avoid AppRegistryNotReady error
+# DON'T REMOVE THE NOQA COMMENT: IT WILL BREAK Django cause models can only be imported after django initalized!
+from back.channels_auth import MultiAuthMiddlewareStack  # noqa: E402
 
 
 def get_urls_patterns():
