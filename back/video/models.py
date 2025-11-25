c7a9953065843f34450df6b07d7186b06e009ef1
Tim Schupp <tim@timschupp.de>
2025-11-25 16:03:33 +0100

    added flag to allow for post randomcalls match-making

--- back/video/models.py
+++ back/video/models.py
@@ -134,8 +134,11 @@ class RandomCallMatching(models.Model):
 
     accepted = models.BooleanField(default=False)  # Both users accepted
     rejected = models.BooleanField(default=False)  # At least one user rejected
-    
+
     expired = models.BooleanField(default=False)  # The match acceptece timed out without a match being performed
+
+    u1_matching_requested = models.BooleanField(default=False)
+    u2_matching_requested = models.BooleanField(default=False)
 
     @property
     def is_processed(self):
