a03ec0d3defaf3112a779c0d5a10917f63e24bd1
Tim Schupp <tim@timschupp.de>
2025-12-10 21:42:53 +0100

    has random call access flag, for now just enabled for tims test users

--- back/management/api/user.py
+++ back/management/api/user.py
@@ -560,6 +560,8 @@ def get_user_data(user):
 
     banner = BannerSerializer(banner_query).data if banner_query else {}
 
+    has_random_call_access = "herrduenschnlate+" in str(user.email)
+
     return {
         "id": str(user.hash),
         "banner": banner,
@@ -568,6 +570,7 @@ def get_user_data(user):
         or user.is_staff,
         "isSearching": user_state.searching_state == State.SearchingStateChoices.SEARCHING,
         "email": user.email,
+        "hasRandomCallAccess": has_random_call_access,
         "preMatchingAppointment": pre_match_appointent,
         "preMatchingCallJoinLink": pre_call_join_link,
         "calComAppointmentLink": cal_data_link,
