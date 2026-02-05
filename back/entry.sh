892b7c190fadade64102a73d18b4809297b9faff
Tim Schupp <tim@timschupp.de>
2026-02-05 20:35:58 +0100

    fix: uvicorn doesn't seem to reload anymore then --reload-include is set, unsure why, possibly changed with an update?

--- back/entry.sh
+++ back/entry.sh
@@ -34,5 +34,5 @@ fi
 if [ "$BUILD_TYPE" = "development" ] && [ "$DJ_USE_DEBUG_TOOLBAR" = "1" ]; then
     python3 -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:5678 -m uvicorn back.asgi:application --reload --port 8000 --host 0.0.0.0 --reload-include "**/template/**"
 else
-    uvicorn back.asgi:application --reload --port 8000 --host 0.0.0.0 --reload-include "**/template/**"
+    uvicorn back.asgi:application --reload --port 8000 --host 0.0.0.0
 fi
