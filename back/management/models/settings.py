c16cd1fe913d3eb356139dfef9ff456a79905993
Tim Schupp <tim@timschupp.de>
2026-03-16 12:03:36 +0100

    remove old comments

--- back/management/models/settings.py
+++ back/management/models/settings.py
@@ -37,11 +37,6 @@ class EmailSettings(models.Model):
             return  # already sent
 
         self.user_form_unfinished_reminder1 = True
-
-        # send groupmail function automaticly checks if users have unsubscribed!
-        # we still mark email verification reminder 1 as True, since we at least tried to send it,
-        # never wanna send twice! Not even **try** twice!
-        # send the mail
         user.send_email_v2("unfinished_user_form_1")
 
         self.save()
@@ -51,10 +46,6 @@ class EmailSettings(models.Model):
             return  # already sent
 
         self.user_form_unfinished_reminder2 = True
-
-        # send groupmail function automaticly checks if users have unsubscribed!
-        # we still mark email verification reminder 1 as True, since we at least tried to send it,
-        # never wanna send twice! Not even **try** twice!
         user.send_email_v2("unfinished_user_form_2")
         self.save()
 
@@ -63,12 +54,6 @@ class EmailSettings(models.Model):
             return  # already sent
 
         self.email_verification_reminder1 = True
-
-        # send groupmail function automaticly checks if users have unsubscribed!
-        # we still mark email verification reminder 1 as True, since we at least tried to send it,
-        # never wanna send twice! Not even **try** twice!
-
-        # send the mail
         user.send_email_v2("verify-email")
         self.save()
 
