e0c4452c28c442ff6419c93d10e99df08a00272d
Tim Schupp <tim@timschupp.de>
2026-04-29 23:13:14 +0200

    draft: improve general redirect behavior

--- back/management/views/main_frontend.py
+++ back/management/views/main_frontend.py
@@ -120,15 +120,8 @@ class MainFrontendRouter(View):
             return redirect("/app/onboarding/")
 
         if (
-            request.user.state.is_email_verified()
-            and request.user.state.is_user_form_filled()
-            and request.user.state.is_onboarded
-            and (not any([path.startswith(p) for p in self.LOGGED_IN_NO_REDIRECT_PATHS]))
+            not any([path.startswith(p) for p in self.LOGGED_IN_NO_REDIRECT_PATHS])
         ):
-            return redirect("/app/")
-
-        if path == "":
-            # Keep authenticated users off the public root and hand off to app routing.
             return redirect("/app/")
 
         extra_template_data = {}
