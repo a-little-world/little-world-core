61f549da3fdb67b2daefbf143acc26d115b4a922
Tim Schupp <tim@timschupp.de>
2026-05-15 06:01:42 +0200

    backfill missing uuid logic

--- back/management/migrations/0154_user_uuid_backfill_and_sync_hash.py
+++ back/management/migrations/0154_user_uuid_backfill_and_sync_hash.py
@@ -3,6 +3,12 @@ import uuid
 from django.db import migrations, models
 
 
+def backfill_user_uuid(apps, schema_editor):
+    User = apps.get_model("management", "User")
+    for user in User.objects.filter(uuid__isnull=True).only("id").iterator(chunk_size=1000):
+        User.objects.filter(pk=user.pk).update(uuid=uuid.uuid4())
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("management", "0153_alter_state_options"),
@@ -12,7 +18,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="user",
             name="uuid",
-            field=models.UUIDField(default=uuid.uuid4, editable=False, null=True, unique=True),
+            field=models.UUIDField(editable=False, null=True),
+        ),
+        migrations.RunPython(backfill_user_uuid, migrations.RunPython.noop),
+        migrations.AlterField(
+            model_name="user",
+            name="uuid",
+            field=models.UUIDField(default=uuid.uuid4, editable=False, unique=True),
         ),
         migrations.AddField(
             model_name="user",
