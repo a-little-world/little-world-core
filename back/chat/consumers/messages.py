38855e96db3918580d06ceac75349cee2ba9fe77
Tim Schupp <tim@timschupp.de>
2026-05-15 07:10:39 +0200

    uuid backward compatablity changes

--- back/chat/consumers/messages.py
+++ back/chat/consumers/messages.py
@@ -28,7 +28,7 @@ class MessageTypes(Enum):
 
 def send_message(user_id, type: MessageTypes, data):
     channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(user_id, data)
+    async_to_sync(channel_layer.group_send)(str(user_id), data)
 
 
 @dataclass
